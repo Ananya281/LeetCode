3b67281a328c3f278feb7156503e04f8704d3b0e
Ananya Chauhan <116284409+Ananya281@users.noreply.github.com>
2024-11-27 11:03:51 +0530

    Time: 0 ms (100.00%) | Memory: 16.5 MB (5.15%) - LeetSync

--- 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.cpp
+++ 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.cpp
@@ -1,3 +1,14 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
     vector<vector<int>> verticalTraversal(TreeNode* root) {
@@ -8,29 +19,29 @@ public:
         {
             auto p=q.front();
             q.pop();
-            TreeNode* node=p.first;
+            root=p.first;
             int x=p.second.first;
             int y=p.second.second;
-            mpp[x][y].insert(node->val);
-            if(node->left!=NULL)
+            mpp[x][y].insert(root->val);
+            if(root->left!=NULL)
             {
-                q.push({node->left,{x-1,y+1}});
+                q.push({root->left,{x-1,y+1}});
             }
-            if(node->right!=NULL)
+            if(root->right!=NULL)
             {
-                q.push({node->right,{x+1,y+1}});
+                q.push({root->right,{x+1,y+1}});
             }
         }
-        vector<vector<int>>ans;
+        vector<vector<int>>result;
         for(auto it:mpp)
         {
-            vector<int>result;
-            for(auto s:it.second)
+            vector<int>ans;
+            for(auto ip:it.second)
             {
-                result.insert(result.end(),s.second.begin(),s.second.end());
+                ans.insert(ans.end(),ip.second.begin(),ip.second.end());
             }
-            ans.push_back(result);
+            result.push_back(ans);
         }
-        return ans;
+        return result;
     }
 };
