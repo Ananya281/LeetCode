09c93732f7f9ca0492e838999cb12405f2e0be27
Ananya Chauhan <116284409+Ananya281@users.noreply.github.com>
2024-11-30 10:14:31 +0530

    Time: 3 ms (79.44%) | Memory: 27.3 MB (66.33%) - LeetSync

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -19,13 +19,13 @@ public:
         }
         return buildtree(preorder,0,preorder.size()-1,inorder,0,inorder.size()-1,mpp);
     }
-    TreeNode* buildtree(vector<int>&preorder,int prestart, int preend,vector<int>&inorder,int instart, int inend, map<int,int>&mpp)
+    TreeNode* buildtree(vector<int>& preorder, int prestart, int preend, vector<int>& inorder, int instart, int inend, map<int,int>&mpp)
     {
         if(prestart>preend||instart>inend)
         {
             return NULL;
         }
-        TreeNode*root=new TreeNode(preorder[prestart]);
+        TreeNode* root=new TreeNode(preorder[prestart]);
         int inroot=mpp[root->val];
         int numleft=inroot-instart;
         root->left=buildtree(preorder,prestart+1,prestart+numleft,inorder,instart,inroot-1,mpp);
