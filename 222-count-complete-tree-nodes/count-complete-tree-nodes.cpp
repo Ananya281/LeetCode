1ff47eadad3c2d70de273e8014d021b87b1627be
Ananya Chauhan <116284409+Ananya281@users.noreply.github.com>
2024-12-08 11:04:34 +0530

    Time: 0 ms (100.00%) | Memory: 32.7 MB (6.90%) - LeetSync

--- 222-count-complete-tree-nodes/count-complete-tree-nodes.cpp
+++ 222-count-complete-tree-nodes/count-complete-tree-nodes.cpp
@@ -11,23 +11,34 @@
  */
 class Solution {
 public:
-    void inorder(TreeNode* root,int &count)
-    {
-        if(root==NULL)
-        {
-            return;
-        }
-        inorder(root->left,count);
-        count++;
-        inorder(root->right,count);
-    }
     int countNodes(TreeNode* root) {
         if(root==NULL)
         {
             return 0;
         }
-        int count=0;
-        inorder(root,count);
-        return count;
+        queue<TreeNode*>q;
+        q.push(root);
+        int total=0;
+        while(!q.empty())
+        {
+            int n=q.size();
+            vector<int>temp;
+            for(int i=0;i<n;i++)
+            {
+                TreeNode*node=q.front();
+                q.pop();
+                if(node->left)
+                {
+                    q.push(node->left);
+                }
+                if(node->right)
+                {
+                    q.push(node->right);
+                }
+                temp.push_back(node->val);
+            }
+            total=total+temp.size();
+        }
+        return total;
     }
 };
