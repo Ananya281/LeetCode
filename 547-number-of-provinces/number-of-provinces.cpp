a6088cfd648b88677e525833bac103f64215a1ed
Ananya Chauhan <116284409+Ananya281@users.noreply.github.com>
2024-12-23 22:37:27 +0530

    Time: 4 ms (34.45%) | Memory: 21.1 MB (14.05%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -1,19 +1,31 @@
 class Solution {
 public:
-    void dfs(int node,vector<vector<int>>&isConnected,vector<int>&vis)
+    void dfs(int node,vector<vector<int>>&adj,vector<int>&vis)
     {
         vis[node]=1;
-        int n=isConnected.size();
-        for(int i=0;i<n;i++)
+        int n=adj.size();
+        for(auto it:adj[node])
         {
-            if(isConnected[node][i]==1&&!vis[i])
+            if(!vis[it])
             {
-                dfs(i,isConnected,vis);
+                dfs(it,adj,vis);
             }
         }
     }
     int findCircleNum(vector<vector<int>>& isConnected) {
         int n=isConnected.size();
+        vector<vector<int>>adj(n);
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(isConnected[i][j]==1&&i!=j)
+                {
+                    adj[i].push_back(j);
+                    adj[j].push_back(i);
+                }
+            }
+        }
         vector<int>vis(n,0);
         int count=0;
         for(int i=0;i<n;i++)
@@ -21,7 +33,7 @@ public:
             if(!vis[i])
             {
                 count++;
-                dfs(i,isConnected,vis);
+                dfs(i,adj,vis);
             }
         }
         return count;
