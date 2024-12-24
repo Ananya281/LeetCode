b20c1ee34c4844139291efb846e3fcbc2b3868ea
Ananya Chauhan <116284409+Ananya281@users.noreply.github.com>
2024-12-24 11:14:22 +0530

    Time: 23 ms (48.80%) | Memory: 38.9 MB (25.13%) - LeetSync

---
+++ 542-01-matrix/01-matrix.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    void bfs(vector<vector<int>>&mat,vector<vector<int>>&vis,vector<vector<int>>&dis,queue<pair<pair<int,int>,int>>&q)
+    {
+        int n=mat.size();
+        int m=mat[0].size();
+        int drow[4]={-1,0,+1,0};
+        int dcol[4]={0,+1,0,-1};
+        while(!q.empty())
+        {
+            int r=q.front().first.first;
+            int c=q.front().first.second;
+            int s=q.front().second;
+            q.pop();
+            dis[r][c]=s;
+            for(int i=0;i<4;i++)
+            {
+                int nrow=r+drow[i];
+                int ncol=c+dcol[i];
+                if(nrow>=0&&nrow<n&&ncol>=0&&ncol<m&&vis[nrow][ncol]!=1)
+                {
+                    q.push({{nrow,ncol},s+1});
+                    vis[nrow][ncol]=1;
+                }
+            }
+        }
+    }
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        int n=mat.size();
+        int m=mat[0].size();
+        vector<vector<int>>vis(n,vector<int>(m));
+        vector<vector<int>>dis(n,vector<int>(m));
+        queue<pair<pair<int,int>,int>>q;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(mat[i][j]==0)
+                {
+                    q.push({{i,j},0});
+                    vis[i][j]=1;
+                }
+            }
+        }
+        bfs(mat,vis,dis,q);
+        return dis;
+    }
+};
