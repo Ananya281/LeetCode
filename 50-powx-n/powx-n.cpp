3b61993bb9757d1abce24dc385a7365386ee2c6b
Ananya Chauhan <116284409+Ananya281@users.noreply.github.com>
2024-12-29 23:03:42 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (42.51%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -1,6 +1,29 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        return pow(x,n);
+        double ans=1;
+        if(n==INT_MIN)
+        {
+            return 1/(myPow(x,INT_MAX)*x);
+        }
+        if(n<0)
+        {
+            x=1/x;
+            n=n*(-1);
+        }
+        while(n>0)
+        {
+            if(n%2==1)
+            {
+                ans=ans*x;
+                n--;
+            }
+            else
+            {
+                n=n/2;
+                x=x*x;
+            }
+        }
+        return ans;
     }
 };
