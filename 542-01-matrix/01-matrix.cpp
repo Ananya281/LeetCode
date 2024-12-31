6b4bae0e1c1ffa292a058a430ce3738c496ae7fc
Ananya Chauhan <116284409+Ananya281@users.noreply.github.com>
2025-01-01 00:34:16 +0530

    Time: 27 ms (35.72%) | Memory: 38.9 MB (21.88%) - LeetSync

--- 542-01-matrix/01-matrix.cpp
+++ 542-01-matrix/01-matrix.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    void bfs(vector<vector<int>>&mat,vector<vector<int>>&vis,vector<vector<int>>&dis,queue<pair<pair<int,int>,int>>&q)
+    void bfs(queue<pair<pair<int,int>,int>>&q,vector<vector<int>>&dis,vector<vector<int>>&vis,vector<vector<int>>&mat)
     {
         int n=mat.size();
         int m=mat[0].size();
@@ -11,16 +11,16 @@ public:
             int r=q.front().first.first;
             int c=q.front().first.second;
             int d=q.front().second;
-            dis[r][c]=d;
             q.pop();
+            dis[r][c]=d;
             for(int i=0;i<4;i++)
             {
                 int nrow=r+drow[i];
                 int ncol=c+dcol[i];
-                if(nrow>=0&&nrow<n&&ncol>=0&&ncol<m&&vis[nrow][ncol]!=1)
+                if(nrow>=0&&nrow<n&&ncol>=0&&ncol<m&&!vis[nrow][ncol])
                 {
-                    q.push({{nrow,ncol},d+1});
                     vis[nrow][ncol]=1;
+                    q.push({{nrow,ncol},d+1});
                 }
             }
         }
@@ -35,17 +35,14 @@ public:
         {
             for(int j=0;j<m;j++)
             {
-                if(mat[i][j]==0)
+                if(mat[i][j]==0&&!vis[i][j])
                 {
-                    if(!vis[i][j])
-                    {
-                        q.push({{i,j},0});
-                        vis[i][j]=1;
-                    }
+                    vis[i][j]=1;
+                    q.push({{i,j},0});
                 }
             }
         }
-        bfs(mat,vis,dis,q);
+        bfs(q,dis,vis,mat);
         return dis;
     }
 };
