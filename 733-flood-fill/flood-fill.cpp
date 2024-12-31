b3ffb6b67ca1a0597014844e70b2746221000c76
Ananya Chauhan <116284409+Ananya281@users.noreply.github.com>
2025-01-01 00:14:47 +0530

    Time: 0 ms (100.00%) | Memory: 18 MB (55.24%) - LeetSync

--- 733-flood-fill/flood-fill.cpp
+++ 733-flood-fill/flood-fill.cpp
@@ -1,37 +1,29 @@
 class Solution {
 public:
-    void bfs(int initial,vector<vector<int>>&image,vector<vector<int>>&result,int sr,int sc,int color)
+    void dfs(int initial,vector<vector<int>>&image,vector<vector<int>>&result,int sr,int sc,int color)
     {
         int n=image.size();
         int m=image[0].size();
         result[sr][sc]=color;
         int drow[4]={-1,0,+1,0};
         int dcol[4]={0,+1,0,-1};
-        queue<pair<int,int>>q;
-        q.push({sr,sc});
-        while(!q.empty())
-        {
-            int r=q.front().first;
-            int c=q.front().second;
-            q.pop();
+
             for(int i=0;i<4;i++)
             {
-                int nrow=r+drow[i];
-                int ncol=c+dcol[i];
+                int nrow=sr+drow[i];
+                int ncol=sc+dcol[i];
                 if(nrow>=0&&nrow<n&&ncol>=0&&ncol<m && result[nrow][ncol]!=color && image[nrow][ncol]==initial)
                 {
-                    q.push({nrow,ncol});
-                    result[nrow][ncol]=color;
+                    dfs(initial,image,result,nrow,ncol,color);
                 }
             }
-        }
     }
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
         int n=image.size();
         int m=image[0].size();
         vector<vector<int>>result=image;
         int initial=image[sr][sc];
-        bfs(initial,image,result,sr,sc,color);
+        dfs(initial,image,result,sr,sc,color);
         return result;
     }
 };
