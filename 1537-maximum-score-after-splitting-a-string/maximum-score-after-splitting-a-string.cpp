2d931b4f07ceece6af9a108c5913a533c85faa25
Ananya Chauhan <116284409+Ananya281@users.noreply.github.com>
2025-01-01 23:52:03 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (38.26%) - LeetSync

--- 1537-maximum-score-after-splitting-a-string/maximum-score-after-splitting-a-string.cpp
+++ 1537-maximum-score-after-splitting-a-string/maximum-score-after-splitting-a-string.cpp
@@ -2,31 +2,28 @@ class Solution {
 public:
     int maxScore(string s) {
         int n=s.size();
-        int i=0;
-        int j=1;
-        int score=0;
-        while(j<n)
+        int count0=0;
+        int count1=0;
+        int maxscore=0;
+        for(auto it:s)
         {
-            int count0=0;
-            int count1=0;
-            for(int r=0;r<=i;r++)
+            if(it=='1')
             {
-                if(s[r]=='0')
-                {
-                    count0++;
-                }
+                count1++;
             }
-            for(int t=j;t<n;t++)
-            {
-                if(s[t]=='1')
-                {
-                    count1++;
-                }
-            }
-            score=max(score,count0+count1);
-            i=j;
-            j++;
         }
-        return score;
+        for(int i=0;i<n-1;i++)
+        {
+            if(s[i]=='0')
+            {
+                count0++;
+            }
+            else
+            {
+                count1--;
+            }
+            maxscore=max(maxscore,count0+count1);
+        }
+        return maxscore;
     }
 };
