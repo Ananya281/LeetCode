cb64ff72f2692e6c16c5da7e99851d9a0e94020b
Ananya Chauhan <116284409+Ananya281@users.noreply.github.com>
2025-01-01 19:26:36 +0530

    Time: 4 ms (12.96%) | Memory: 19 MB (7.88%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.cpp
+++ 801-is-graph-bipartite/is-graph-bipartite.cpp
@@ -3,23 +3,24 @@ public:
     bool bfs(int node,int color1,vector<vector<int>>&graph,vector<int>&color)
     {
         int n=graph.size();
-        queue<int>q;
-        q.push(node);
+        queue<pair<int,int>>q;
+        q.push({node,color1});
         color[node]=color1;
         while(!q.empty())
         {
-            int ele=q.front();
+            int ele=q.front().first;
+            int c=q.front().second;
             q.pop();
             for(auto it:graph[ele])
             {
                 if(color[it]==-1)
                 {
-                    if(!bfs(it,!color1,graph,color))
+                    if(!bfs(it,!c,graph,color))
                     {
                         return false;
                     }
                 }
-                else if(color[it]==color1)
+                else if(color[it]==c)
                 {
                     return false;
                 }
