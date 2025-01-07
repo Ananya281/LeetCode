c7a5484c4ceabaf1e6632a97315b9fbfb7da9a44
Ananya Chauhan <116284409+Ananya281@users.noreply.github.com>
2025-01-07 17:07:46 +0530

    Time: 0 ms (100.00%) | Memory: 11.4 MB (43.54%) - LeetSync

--- 1524-string-matching-in-an-array/string-matching-in-an-array.cpp
+++ 1524-string-matching-in-an-array/string-matching-in-an-array.cpp
@@ -1,22 +1,27 @@
 class Solution {
 public:
     vector<string> stringMatching(vector<string>& words) {
-        int n = words.size();
-        vector<string> result;
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (i == j || words[i].empty() || words[j].empty()) {
-                    continue; // Skip same words and empty strings
+        int n=words.size();
+        vector<string>result;
+        //O(n*n*m)
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(i==j||words[i].empty()||words[j].empty())
+                {
+                    continue;
                 }
-                if (words[j].find(words[i]) != string::npos) { // Check if words[i] is a substring of words[j]
+                //to check if words[i] is a substring of words[]
+                //find function take time of word's length, if it is m, then  O(m)
+                if(words[j].find(words[i])!=string::npos)//string::npos is constant indicating "not found"
+                {
                     result.push_back(words[i]);
-                    break; // Break as we found a match for words[i]
+                    break;
                 }
             }
         }
-
-        // Return the result vector
         return result;
     }
 };
+// 100*100*30=3*10^5
