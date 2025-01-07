b877f1ea6d38fe05c3d1a1f14fe12a967ac66c4c
Ananya Chauhan <116284409+Ananya281@users.noreply.github.com>
2025-01-07 18:07:17 +0530

    Time: 11 ms (10.89%) | Memory: 15.3 MB (7.75%) - LeetSync

--- 1524-string-matching-in-an-array/string-matching-in-an-array.cpp
+++ 1524-string-matching-in-an-array/string-matching-in-an-array.cpp
@@ -1,20 +1,78 @@
 class Solution {
 public:
+    bool kmp(string &pattern,string &text)
+    {
+        int m=pattern.size();
+        int n=text.size();
+        vector<int>lps=buildlps(pattern);
+        int i=0;
+        int j=0;
+        while(i<n)
+        {
+            if(pattern[j]==text[i])
+            {
+                i++;
+                j++;
+            }
+            if(j==m)
+            {
+                return true;
+            }
+            else if(i<n && pattern[j]!=text[i])
+            {
+                if(j!=0)
+                {
+                    j=lps[j-1];
+                }
+                else
+                {
+                    i++;
+                }
+            }
+        }
+        return false;
+    }
+    vector<int>buildlps(string &pattern)
+    {
+        int n=pattern.size();
+        int len=0;
+        vector<int>lps(n,0);
+        int i=1;
+        while(i<n)
+        {
+            if(pattern[i]==pattern[len])
+            {
+                len++;
+                lps[i]=len;
+                i++;
+            }
+            else
+            {
+                if(len!=0)
+                {
+                    len=lps[len-1];
+                }
+                else
+                {
+                    lps[i]=0;
+                    i++;
+                }
+            }
+        }
+        return lps;
+    }
     vector<string> stringMatching(vector<string>& words) {
         int n=words.size();
         vector<string>result;
-        //O(n*n*m)
+        sort(words.begin(),words.end(),[](const string &a,const string &b)
+        {
+            return a.size()<b.size();
+        });
         for(int i=0;i<n;i++)
         {
-            for(int j=0;j<n;j++)
+            for(int j=i+1;j<n;j++)
             {
-                if(i==j||words[i].empty()||words[j].empty())
-                {
-                    continue;
-                }
-                //to check if words[i] is a substring of words[]
-                //find function take time of word's length, if it is m, then  O(m)
-                if(words[j].find(words[i])!=string::npos)//string::npos is constant indicating "not found"
+                if(kmp(words[i],words[j]))
                 {
                     result.push_back(words[i]);
                     break;
@@ -24,4 +82,3 @@ public:
         return result;
     }
 };
-// 100*100*30=3*10^5
