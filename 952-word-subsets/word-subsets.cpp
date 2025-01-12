f51c7219d8e024ba3c3c8c651cdcdcba467d590d
Ananya Chauhan <116284409+Ananya281@users.noreply.github.com>
2025-01-12 12:44:58 +0530

    Time: 349 ms (6.00%) | Memory: 169.8 MB (5.28%) - LeetSync

--- 952-word-subsets/word-subsets.cpp
+++ 952-word-subsets/word-subsets.cpp
@@ -1,34 +1,37 @@
 class Solution {
 public:
-    map<char,int>countalphabet(string& str)
+map<char,int>countalp(string &str)
+{
+    map<char,int>mpp;
+    for(auto it:str)
     {
-        map<char,int>ans;
-        for(auto c:str)
-        {
-            ans[c]++;
-        }
-        return ans;
+        mpp[it]++;
     }
+    return mpp;
+}
+void mapform(string str,map<char,int>&mpp)
+{
+    map<char,int>mpptest=countalp(str);
+    for(auto it:mpptest)
+    {
+        mpp[it.first]=max(mpp[it.first],mpptest[it.first]);
+    }
+}
     vector<string> wordSubsets(vector<string>& words1, vector<string>& words2) {
-        map<char,int>words2max;
+        map<char,int>mpp2;
         for(auto it:words2)
         {
-            map<char,int>words2count=countalphabet(it);
-            for(auto pair:words2count)
-            {
-                char c=pair.first;
-                words2max[c]=max(words2max[c],words2count[c]);
-            }
+            mapform(it,mpp2);
         }
-        vector<string>ans;
+        vector<string>result;
         for(auto it:words1)
         {
-            map<char,int>words1count=countalphabet(it);
+            map<char,int>mpp1=countalp(it);
             bool subset=true;
-            for(auto pair:words2max)
+            for(auto p:mpp2)
             {
-                char c=pair.first;
-                if(words1count[c]<words2max[c])
+                char c=p.first;
+                if(mpp1[c]<mpp2[c])
                 {
                     subset=false;
                     break;
@@ -36,9 +39,9 @@ public:
             }
             if(subset==true)
             {
-                ans.push_back(it);
+                result.push_back(it);
             }
         }
-        return ans;
+        return result;
     }
 };
