0f2e9161b41917509e3ddf9007885a086011474c
Ananya Chauhan <116284409+Ananya281@users.noreply.github.com>
2025-01-17 16:27:18 +0530

    Update bitwise-and-of-numbers-range.cpp

--- 201-bitwise-and-of-numbers-range/bitwise-and-of-numbers-range.cpp
+++ 201-bitwise-and-of-numbers-range/bitwise-and-of-numbers-range.cpp
@@ -8,3 +8,11 @@ public:
         return right;
     }
 };
+
+
+//moving from left to right results TLE, since calculating the bitwise AND (left=left&(left+1)), process each number in range [left,right]
+//so when range is large, gives TLE
+
+//moving from right to left, it quickly aligns their common bits by removing the different bits
+//approach onky depends on the number of different bits, which is smaller than the range size
+//reduces the number of operations, making it faster 
