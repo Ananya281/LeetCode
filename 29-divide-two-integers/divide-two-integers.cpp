d0dc31304a6b2c9f6f54ed75d80ec000d0912d51
Ananya Chauhan <116284409+Ananya281@users.noreply.github.com>
2025-01-19 22:59:26 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (65.70%) - LeetSync

--- 29-divide-two-integers/divide-two-integers.cpp
+++ 29-divide-two-integers/divide-two-integers.cpp
@@ -1,31 +1,27 @@
 class Solution {
 public:
     int divide(int dividend, int divisor) {
-        if(dividend==divisor)
+        if(divisor==dividend)
         {
             return 1;
         }
         bool sign=true;
-        if(dividend>0 && divisor<0)
+        if((dividend>=0 && divisor<0) || (dividend<=0 && divisor>0))
         {
             sign=false;
         }
-        if(dividend<0 && divisor>0)
-        {
-            sign=false;
-        }
-        long n = abs((long)dividend);
-        long d = abs((long)divisor);
+        long n=abs(long(dividend));
+        long d=abs(long(divisor));
         int ans=0;
         while(n>=d)
         {
             int count=0;
-            while(n >= (d << (count+1)))
+            while(n>=(d<<(count+1)) && count<31)
             {
                 count++;
             }
-            ans = ans + (1<<count);
-            n = n - (d<<count);
+            ans=ans+(1<<count);
+            n=n-(d<<count);
         }
         if(ans==(1<<31) && sign==true)
         {
@@ -35,13 +31,13 @@ public:
         {
             return INT_MIN;
         }
-        if(sign==true)
+        if(sign==false)
         {
-            return ans;
+            return -ans;
         }
         else
         {
-            return -ans;
+            return ans;
         }
     }
 };
