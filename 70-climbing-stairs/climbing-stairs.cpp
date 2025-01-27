cd330340bdc203e9db6e0fb666bfbea62b9dfc3d
Ananya Chauhan <116284409+Ananya281@users.noreply.github.com>
2025-01-27 23:02:51 +0530

    Time: 1 ms (7.05%) | Memory: 8.6 MB (35.27%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
-    int top(vector<int>&dp,int n)
+int racoon(vector<int>&dp,int n)
+{
+    if(n==0 || n==1)
     {
-        if(n==0||n==1)
-        {
-            return 1;
-        }
-        if(dp[n]!=0)
-        {
-            return dp[n];
-        }
-        return dp[n]=top(dp,n-1)+top(dp,n-2);
+        return 1;
     }
+    if(dp[n]!=-1)
+    {
+        return dp[n];
+    }
+    return dp[n]=racoon(dp,n-1)+racoon(dp,n-2);
+}
     int climbStairs(int n) {
-       vector<int>dp(n+1,0);
-       return top(dp,n);
+        vector<int>dp(n+1,-1);
+        return racoon(dp,n);
     }
 };
