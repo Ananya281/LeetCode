2816c20a2a976aa52e5e6c9952639dca4b327ba2
Ananya Chauhan <116284409+Ananya281@users.noreply.github.com>
2025-01-27 23:25:13 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (35.27%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,5 +1,32 @@
 class Solution {
 public:
+int space(int n)
+{
+    if(n==0 || n==1)
+    {
+        return 1;
+    }
+    int prev=1;
+    int prev2=1;
+    int curr;
+    for(int i=2;i<=n;i++)
+    {
+        curr=prev+prev2;
+        prev2=prev;
+        prev=curr;
+    }
+    return curr;
+}
+int tabu(vector<int>&dp,int n)
+{
+    dp[0]=1;
+    dp[1]=1;
+    for(int i=2;i<=n;i++)
+    {
+        dp[i]=dp[i-1]+dp[i-2];
+    }
+    return dp[n];
+}
 int racoon(vector<int>&dp,int n)
 {
     if(n==0 || n==1)
@@ -14,6 +41,12 @@ int racoon(vector<int>&dp,int n)
 }
     int climbStairs(int n) {
         vector<int>dp(n+1,-1);
-        return racoon(dp,n);
+        // int r=racoon(dp,n);
+        // int t=tabu(dp,n);
+        int s=space(n);
+        return s;
     }
 };
+// 0 1 2 3 4 5 6 
+// 0 1 1
+// 0=prev2 1=prev 2=curr
