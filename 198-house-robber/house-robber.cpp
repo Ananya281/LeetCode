cae3e4d167594f2735fa080c7cbbf08173adecb6
Ananya Chauhan <116284409+Ananya281@users.noreply.github.com>
2025-02-01 12:38:34 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (21.94%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -19,10 +19,30 @@ public:
         dp[n]=max(pick,nonpick);
         return dp[n];
     }
+    int tabu(vector<int>&dp,int n,vector<int>&nums)
+    {
+        if(n==1)
+        {
+            return nums[0];
+        }
+        dp[0]=nums[0];
+        for(int i=1;i<n;i++)
+        {
+            int pick=nums[i];
+            if(i>1)
+            {
+                pick=pick+dp[i-2];
+            }
+            int nonpick=dp[i-1];
+            dp[i]=max(pick,nonpick);
+        }
+        return dp[n-1];
+    }
     int rob(vector<int>& nums) {
         int n=nums.size();
         vector<int>dp(n,-1);
-        int ans1=memo(dp,n-1,nums);
-        return ans1;
+        // int ans1=memo(dp,n-1,nums);
+        int ans2=tabu(dp,n,nums);
+        return ans2;
     }
 };
