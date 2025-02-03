7a981d9b37b8876d0cbd27890e3070a23862bd6e
Ananya Chauhan <116284409+Ananya281@users.noreply.github.com>
2025-02-03 21:37:01 +0530

    Time: 120 ms (57.84%) | Memory: 13.4 MB (91.85%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -22,6 +22,57 @@ public:
         }
         return dp[ind][target]=take||nontake;
     }
+    bool tabu(int n,int target,vector<vector<bool>>&dp,vector<int>&nums)
+    {
+        for(int i=0;i<n;i++)
+        {
+            dp[i][0]=true;
+        }
+        if(nums[0]<=target)
+        {
+            dp[0][nums[0]]=true;
+        }
+        for(int i=1;i<n;i++)
+        {
+            for(int k=1;k<=target;k++)
+            {
+                bool nontake=dp[i-1][k];
+                bool take=false;
+                if(nums[i]<=k)
+                {
+                    take=dp[i-1][k-nums[i]];
+                }
+                dp[i][k]=take||nontake;
+            }
+        }
+        return dp[n-1][target];
+    }
+    int space(int n,int target,vector<int>&nums)
+    {
+        vector<bool>prev(target+1,false);
+        vector<bool>curr(target+1,false);
+        prev[0]=true;
+        curr[0]=true;
+        if(nums[0]<=target)
+        {
+            prev[nums[0]]=true;
+        }
+        for(int i=1;i<n;i++)
+        {
+            for(int k=1;k<=target;k++)
+            {
+                bool nottake=prev[k];
+                bool take=false;
+                if(nums[i]<=k)
+                {
+                    take=prev[k-nums[i]];
+                }
+                curr[k]=take||nottake;
+            }
+            prev=curr;
+        }
+        return prev[target];
+    }
     bool canPartition(vector<int>& nums) {
         int n=nums.size();
         int total=0;
@@ -34,7 +85,10 @@ public:
             return false;
         }
         int sum=total/2;
-        vector<vector<int>>dp(n,vector<int>(sum+1,-1));
-        return memo(n-1,sum,dp,nums);
+        // vector<vector<int>>dp(n,vector<int>(sum+1,-1));
+        // return memo(n-1,sum,dp,nums);
+        // vector<vector<bool>>dp(n,vector<bool>(sum+1,false));
+        // return tabu(n,sum,dp,nums);
+        return space(n,sum,nums);
     }
 };
