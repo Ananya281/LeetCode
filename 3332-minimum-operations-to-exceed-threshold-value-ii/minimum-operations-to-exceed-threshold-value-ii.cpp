90e11f5c53c87f20b3baf0807f4ffe34f0a1ffac
Ananya Chauhan <116284409+Ananya281@users.noreply.github.com>
2025-02-13 10:26:34 +0530

    Time: 96 ms (59.95%) | Memory: 99 MB (39.26%) - LeetSync

---
+++ 3332-minimum-operations-to-exceed-threshold-value-ii/minimum-operations-to-exceed-threshold-value-ii.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums, int k) {
+        int n=nums.size();
+        priority_queue<long long,vector<long long>,greater<long long>>pq;
+        for(int i=0;i<n;i++)
+        {
+            pq.push(nums[i]);
+        }
+        int op=0;
+        while(pq.size()>1 && pq.top()<k)
+        {
+            long long first=pq.top();
+            pq.pop();
+            long long second=pq.top();
+            pq.pop();
+            long long num=first*2+second;
+            pq.push(num);
+            op++;
+        }
+        return op;
+    }
+};
