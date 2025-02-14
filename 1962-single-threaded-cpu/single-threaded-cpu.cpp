4308f70b1eb628edf6ea084bc1bba46bfc2f7cba
Ananya Chauhan <116284409+Ananya281@users.noreply.github.com>
2025-02-14 08:26:43 +0530

    Time: 80 ms (89.07%) | Memory: 132.1 MB (81.97%) - LeetSync

---
+++ 1962-single-threaded-cpu/single-threaded-cpu.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<int> getOrder(vector<vector<int>>& tasks) {
+        int n=tasks.size();
+        vector<array<int,3>>sorted;
+        for(int i=0;i<n;i++)
+        {
+            int st=tasks[i][0];
+            int pre=tasks[i][1];
+            sorted.push_back({st,pre,i});
+        }
+        sort(sorted.begin(),sorted.end());
+        vector<int>result;
+        long long curr=0;
+        int ind=0;
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
+        while(ind<n || !pq.empty())
+        {
+            if(pq.empty() && curr<sorted[ind][0])
+            {
+                curr=sorted[ind][0];
+            }
+            while(ind<n && sorted[ind][0]<=curr)
+            {
+                pq.push({sorted[ind][1],sorted[ind][2]});
+                ind++;
+            }
+            pair<int,int>currtask=pq.top();
+            pq.pop();
+            curr=curr+currtask.first;
+            result.push_back(currtask.second);
+        }
+        return result;
+    }
+};
