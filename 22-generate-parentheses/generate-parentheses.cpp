1913a14a158ac567a96057c78d70a1c4df046e95
Ananya Chauhan <116284409+Ananya281@users.noreply.github.com>
2025-02-21 21:08:54 +0530

    Time: 5 ms (19.39%) | Memory: 13.2 MB (77.52%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.cpp
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -1,31 +1,47 @@
 class Solution {
 public:
     vector<string>result;
-    void solve(string&curr,int n,int open,int close)
+    bool isvalid(string&str)
+    {
+        int count=0;
+        for(auto it:str)
+        {
+            if(it=='(')
+            {
+                count++;
+            }
+            else
+            {
+                count--;
+            }
+            if(count<0)
+            {
+                return false;
+            }
+        }
+        return count==0;
+    }
+    void solve(string&curr,int n)
     {
         if(curr.size()==2*n)
         {
-            result.push_back(curr);
+            if(isvalid(curr))
+            {
+                result.push_back(curr);
+            }
             return;
         }
-        if(open<n)
-        {
-            curr.push_back('(');
-            solve(curr,n,open+1,close);
-            curr.pop_back();
-        }
-        if(close<open)
-        {
-            curr.push_back(')');
-            solve(curr,n,open,close+1);
-            curr.pop_back();
-        }
+        curr.push_back('(');
+        solve(curr,n);
+        curr.pop_back();
+
+        curr.push_back(')');
+        solve(curr,n);
+        curr.pop_back();
     }
     vector<string> generateParenthesis(int n) {
         string curr="";
-        int open=0;
-        int close=0;
-        solve(curr,n,open,close);
+        solve(curr,n);
         return result;
     }
 };
