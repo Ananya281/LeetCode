7576b3aab6b761b2abf67c88e23fc2a1937a252e
Ananya Chauhan <116284409+Ananya281@users.noreply.github.com>
2025-04-15 17:12:19 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (17.45%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
-    int top(vector<int>&dp,int n)
+    int memo(vector<int>&dp,int n)
     {
-        if(n==0 || n==1)
+        if(n==0||n==1)
         {
             return 1;
         }
@@ -10,10 +10,21 @@ public:
         {
             return dp[n];
         }
-        return dp[n]=top(dp,n-1)+top(dp,n-2);
+        dp[n]=memo(dp,n-1)+memo(dp,n-2);
+        return dp[n];
+    }
+    int tabu(vector<int>&dp,int n)
+    {
+        dp[0]=dp[1]=1;
+        for(int i=2;i<=n;i++)
+        {
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
     }
     int climbStairs(int n) {
         vector<int>dp(n+1,-1);
-        return top(dp,n);
+        // return memo(dp,n);  
+        return tabu(dp,n);      
     }
 };
