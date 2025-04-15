19c952f3b910757de4d458c20d930e6847d6434b
Ananya Chauhan <116284409+Ananya281@users.noreply.github.com>
2025-04-15 17:15:03 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (17.45%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -22,9 +22,27 @@ public:
         }
         return dp[n];
     }
+    int space(int n)
+    {
+        int prev2=1;
+        int prev1=1;
+        int curr;
+        if(n==0||n==1)
+        {
+            return 1;
+        }
+        for(int i=2;i<=n;i++)
+        {
+            curr=prev2+prev1;
+            prev2=prev1;
+            prev1=curr;
+        }
+        return prev1;
+    }
     int climbStairs(int n) {
         vector<int>dp(n+1,-1);
         // return memo(dp,n);  
-        return tabu(dp,n);      
+        // return tabu(dp,n);
+        return space(n);      
     }
 };
