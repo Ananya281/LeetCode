8da4d5108c945af10216e1b3f16c420d80cb7c30
Ananya Chauhan <116284409+Ananya281@users.noreply.github.com>
2025-04-17 12:19:28 +0530

    Time: 0 ms (100.00%) | Memory: 52.3 MB (64.30%) - LeetSync

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -5,11 +5,15 @@ public:
         int final=n-1;
         for(int i=n-2;i>=0;i--)
         {
-            if((i+nums[i])>=final)
+            if(i+nums[i]>=final)
             {
                 final=i;
             }
         }
-        return final==0;
+        if(final==0)
+        {
+            return true;
+        }
+        return false;
     }
 };
