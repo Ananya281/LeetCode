f27b085b244d7d3f74989060bfb5ab89acf6757f
Ananya Chauhan <116284409+Ananya281@users.noreply.github.com>
2025-04-18 12:54:14 +0530

    Time: 0 ms (100.00%) | Memory: 12.9 MB (65.81%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -1,24 +1,49 @@
 class Solution {
 public:
-    int memo(int i,int j,vector<vector<int>>&triangle,vector<vector<int>>&dp)
+    int memo(int i, int j, vector<vector<int>>&dp, vector<vector<int>>&triangle)
     {
         int n=triangle.size();
         if(i==n-1)
         {
-            return triangle[i][j];
+            return triangle[n-1][j];
         }
         if(dp[i][j]!=-1)
         {
             return dp[i][j];
         }
-        int d=triangle[i][j]+memo(i+1,j,triangle,dp);
-        int dg=triangle[i][j]+memo(i+1,j+1,triangle,dp);
-        dp[i][j]=min(d,dg);
+        int down=triangle[i][j] + memo(i+1,j,dp,triangle);
+        int dj=triangle[i][j] + memo(i+1,j+1,dp,triangle);
+        dp[i][j]=min(down,dj);
         return dp[i][j];
+    }
+    int tabu(vector<vector<int>>&dp,vector<vector<int>>&triangle)
+    {
+        int n=triangle.size();
+        dp[0][0]=triangle[0][0];
+        int mini=INT_MAX;
+        for(int i=1;i<n;i++)
+        {
+            for(int j=0;j<=i;j++)
+            {
+                int up = INT_MAX, ld = INT_MAX;
+
+                if (j < i) up = triangle[i][j] + dp[i - 1][j]; // from directly above
+                if (j > 0) ld = triangle[i][j] + dp[i - 1][j - 1]; // from left diagonal);
+                dp[i][j]=min(up,ld);
+            }
+        }
+        // return *min_element(dp[n - 1].begin(), dp[n - 1].begin() + n);
+        int s=dp[n-1].size();
+        for(int i=0;i<s;i++)
+        {   
+            mini=min(mini,dp[n-1][i]);
+        }
+        return mini;
     }
     int minimumTotal(vector<vector<int>>& triangle) {
         int n=triangle.size();
         vector<vector<int>>dp(n,vector<int>(n,-1));
-        return memo(0,0,triangle,dp);
+        // return memo(0,0,dp,triangle);
+        return tabu(dp,triangle);
     }
 };
