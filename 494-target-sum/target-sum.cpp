9548fd641554e00226b07061739eb9b72e3d898c
Ananya Chauhan <116284409+Ananya281@users.noreply.github.com>
2025-04-21 10:46:27 +0530

    Time: 5 ms (75.57%) | Memory: 15.2 MB (59.13%) - LeetSync

---
+++ 494-target-sum/target-sum.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    int memo(int i,int target,vector<vector<int>>&dp,vector<int>&nums)
+    {
+        if(i==0)
+        {
+            if(nums[0]==0 && target==0)
+            {
+                return 2;
+            }
+            if(target == 0 || nums[0] == target)
+            {
+                return 1;
+            }
+            return 0;
+        }
+        if(dp[i][target]!=-1)
+        {
+            return dp[i][target];
+        }
+        int nonpick=memo(i-1,target,dp,nums);
+        int pick=0;
+        if(nums[i]<=target)
+        {
+            pick=memo(i-1,target-nums[i],dp,nums);
+        }
+        dp[i][target]=pick+nonpick;
+        return dp[i][target];
+    }
+    int findTargetSumWays(vector<int>& nums, int target) {
+        int n=nums.size();
+        int total=0;
+        for(int i=0;i<n;i++)
+        {
+            total=total+nums[i];
+        }
+        int s2=(total-target)/2;
+        if((total-target)<0 || (total-target)%2==1)
+        {
+            return 0;
+        }
+        vector<vector<int>>dp(n,vector<int>(s2+1,-1));
+        return memo(n-1,s2,dp,nums);
+    }
+};
