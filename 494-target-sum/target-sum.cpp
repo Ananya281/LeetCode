71c699adfd90b57cd89345a6d410879addb15e9e
Ananya Chauhan <116284409+Ananya281@users.noreply.github.com>
2025-04-21 11:06:13 +0530

    Time: 3 ms (88.42%) | Memory: 15 MB (60.89%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -27,6 +27,66 @@ public:
         dp[i][target]=pick+nonpick;
         return dp[i][target];
     }
+    int tabu(int n,int target,vector<vector<int>>&dp,vector<int>&nums)
+    {
+        if(nums[0]==0)
+        {
+            dp[0][0]=2;
+        }
+        else
+        {
+            dp[0][0]=1;
+        }
+        if(nums[0]!=0 && nums[0]<=target)
+        {
+            dp[0][nums[0]]=1;
+        }
+        for(int i=1;i<n;i++)
+        {
+            for(int j=0;j<=target;j++)
+            {
+                int nonpick=dp[i-1][j];
+                int pick=0;
+                if(nums[i]<=j)
+                {
+                    pick=dp[i-1][j-nums[i]];
+                }
+                dp[i][j]=pick+nonpick;
+            }
+        }
+        return dp[n-1][target];
+    }
+    int space(int n,int target,vector<int>&prev,vector<int>&nums)
+    {
+        if(nums[0]==0)
+        {
+            prev[0]=2;
+        }
+        else
+        {
+            prev[0]=1;
+        }
+        if(nums[0]!=0 && nums[0]<=target)
+        {
+            prev[nums[0]]=1;
+        }
+        for(int i=1;i<n;i++)
+        {
+            vector<int>temp(target+1,0);
+            for(int j=0;j<=target;j++)
+            {
+                int nonpick=prev[j];
+                int pick=0;
+                if(nums[i]<=j)
+                {
+                    pick=prev[j-nums[i]];
+                }
+                temp[j]=pick+nonpick;
+            }
+            prev=temp;
+        }
+        return prev[target];
+    }
     int findTargetSumWays(vector<int>& nums, int target) {
         int n=nums.size();
         int total=0;
@@ -39,7 +99,11 @@ public:
         {
             return 0;
         }
-        vector<vector<int>>dp(n,vector<int>(s2+1,-1));
-        return memo(n-1,s2,dp,nums);
+        // vector<vector<int>>dp(n,vector<int>(s2+1,-1));
+        // return memo(n-1,s2,dp,nums);
+        // vector<vector<int>>dp(n,vector<int>(s2+1,0));
+        // return tabu(n,s2,dp,nums);
+        vector<int>dp(s2+1,0);
+        return space(n,s2,dp,nums);
     }
 };
