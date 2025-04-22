71d1399f36da683006887b544b2c2f280dd51faf
Ananya Chauhan <116284409+Ananya281@users.noreply.github.com>
2025-04-22 12:35:31 +0530

    Time: 19 ms (84.12%) | Memory: 26.7 MB (77.07%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -47,13 +47,43 @@ public:
         }
         return dp[n][m];
     }
+    int space(int n,int m,vector<int>&prev,string text1,string text2)
+    {
+        for(int i=0;i<=m;i++)
+        {
+            prev[i]=0;
+        }
+        for(int i=1;i<=n;i++)
+        {
+            vector<int>temp(m+1,-1);
+            temp[0]=0;
+            for(int j=1;j<=m;j++)
+            {
+                if(text1[i-1]==text2[j-1])
+                {
+                    temp[j]=1+prev[j-1];
+                }
+                else
+                {
+                    int up=prev[j];
+                    int left=temp[j-1];
+                    temp[j]=max(up,left);
+                }
+            }
+            prev=temp;
+        }
+        return prev[m];
+    }
     int longestCommonSubsequence(string text1, string text2) {
         int n=text1.size();
         int m=text2.size();
         // vector<vector<int>>dp(n,vector<int>(m,-1));
         // return memo(n-1,m-1,dp,text1,text2);
 
-        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
-        return tabu(n,m,dp,text1,text2);
+        // vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
+        // return tabu(n,m,dp,text1,text2);
+
+        vector<int>dp(m+1,-1);
+        return space(n,m,dp,text1,text2);
     }
 };
