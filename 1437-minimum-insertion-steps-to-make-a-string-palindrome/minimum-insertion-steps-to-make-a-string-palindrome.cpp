e0c9f292f8d747b6818f11d881e988bf4da583ad
Ananya Chauhan <116284409+Ananya281@users.noreply.github.com>
2025-04-23 11:00:26 +0530

    Time: 35 ms (49.72%) | Memory: 31.8 MB (5.02%) - LeetSync

---
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    int memo(int i,int j,vector<vector<int>>&dp,string s1,string s2)
+    {
+        if(i<0||j<0)
+        {
+            return 0;
+        }
+        if(dp[i][j]!=-1)
+        {
+            return dp[i][j];
+        }
+        if(s1[i]==s2[j])
+        {
+            return 1+memo(i-1,j-1,dp,s1,s2);
+        }
+        int up=memo(i-1,j,dp,s1,s2);
+        int left=memo(i,j-1,dp,s1,s2);
+        return dp[i][j]=max(up,left);
+    }
+    int tabu(int n,int m,vector<vector<int>>&dp,string s1,string s2)
+    {
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=m;j++)
+            {
+                if(s1[i-1]==s2[j-1])
+                {
+                    dp[i][j]=1+dp[i-1][j-1];
+                }
+                else
+                {
+                    int up=dp[i-1][j];
+                    int left=dp[i][j-1];
+                    dp[i][j]=max(up,left);
+                }
+            }
+        }
+        return dp[n][m];
+    }
+    int minInsertions(string s) {
+        int n=s.size();
+        string rev=s;
+        reverse(rev.begin(),rev.end());
+        // vector<vector<int>>dp(n,vector<int>(n,-1));
+        // return n-memo(n-1,n-1,dp,s,rev);
+        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+        return n-tabu(n,n,dp,s,rev);
+    }
+};
