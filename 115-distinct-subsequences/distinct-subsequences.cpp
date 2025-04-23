4304fd0c7ec88105cdd561c2ae9ca7760e9466f3
Ananya Chauhan <116284409+Ananya281@users.noreply.github.com>
2025-04-23 13:02:30 +0530

    Time: 23 ms (79.20%) | Memory: 43.7 MB (43.15%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -42,12 +42,36 @@ public:
         }
         return (int)dp[n][m];
     }
+    int space(int n,int m,vector<double>&dp,string s, string t)
+    {
+        dp[0]=1;
+        for(int i=1;i<=n;i++)
+        {
+            vector<double>temp(m+1,0);
+            temp[0]=1;
+            for(int j=1;j<=m;j++)
+            {
+                if(s[i-1]==t[j-1])
+                {
+                    temp[j]=dp[j-1]+dp[j];
+                }
+                else
+                {
+                    temp[j]=dp[j];
+                }
+            }
+            dp=temp;
+        }
+        return (int)dp[m];
+    }
     int numDistinct(string s, string t) {
         int n=s.size();
         int m=t.size();
         // vector<vector<int>>dp(n+1,vector<int>(m,-1));
         // return memo(n-1,m-1,dp,s,t);
-        vector<vector<double>>dp(n+1,vector<double>(m+1,0));
-        return tabu(n,m,dp,s,t);
+        // vector<vector<double>>dp(n+1,vector<double>(m+1,0));
+        // return tabu(n,m,dp,s,t);
+        vector<double>dp(m+1,0);
+        return space(n,m,dp,s,t);
     }
 };
