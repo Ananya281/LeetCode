b4d46d644eb67bffe6c1ea591f35a4a337c4a2b0
Ananya Chauhan <116284409+Ananya281@users.noreply.github.com>
2025-04-24 12:16:43 +0530

    Time: 15 ms (20.15%) | Memory: 13.3 MB (25.63%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -24,10 +24,41 @@ public:
         dp[i][j]=1+min(ins,min(del,rep));
         return dp[i][j];
     }
+    int tabu(int n,int m,vector<vector<int>>&dp,string word1,string word2)
+    {
+        for(int i=0;i<=m;i++)
+        {
+            dp[0][i]=i;
+        }
+        for(int j=0;j<=n;j++)
+        {
+            dp[j][0]=j;
+        }
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=m;j++)
+            {
+                if(word1[i-1]==word2[j-1])
+                {
+                    dp[i][j]=dp[i-1][j-1];
+                }
+                else
+                {
+                    int ins=dp[i][j-1];
+                    int del=dp[i-1][j];
+                    int rep=dp[i-1][j-1];
+                    dp[i][j]=1+min({ins,del,rep});
+                }
+            }
+        }
+        return dp[n][m];
+    }
     int minDistance(string word1, string word2) {
         int n=word1.size();
         int m=word2.size();
-        vector<vector<int>>dp(n,vector<int>(m,-1));
-        return memo(n-1,m-1,dp,word1,word2);
+        // vector<vector<int>>dp(n,vector<int>(m,-1));
+        // return memo(n-1,m-1,dp,word1,word2);
+        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
+        return tabu(n,m,dp,word1,word2);
     }
 };
