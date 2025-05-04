3a74d7fcbfd84a96eb5cbf0ff9d6983e2ba89336
Ananya Chauhan <116284409+Ananya281@users.noreply.github.com>
2025-05-04 11:50:09 +0530

    Time: 20 ms (73.76%) | Memory: 14 MB (48.10%) - LeetSync

--- 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.cpp
+++ 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.cpp
@@ -19,12 +19,38 @@ public:
         dp[i][j]=mini;
         return dp[i][j];
     }
+    int tabu(int c,vector<vector<int>>&dp,vector<int>&cuts)
+    {
+        for(int i=c;i>=1;i--)
+        {
+            for(int j=1;j<=c;j++)
+            {
+                if(i>j)
+                {
+                    continue;
+                }
+                else
+                {
+                    int mini=INT_MAX;
+                    for(int ind=i;ind<=j;ind++)
+                    {
+                        int ans=cuts[j+1]-cuts[i-1]+dp[i][ind-1]+dp[ind+1][j];
+                        mini=min(mini,ans);
+                    }
+                    dp[i][j]=mini;
+                }
+            }
+        }
+        return dp[1][c];
+    }
     int minCost(int n, vector<int>& cuts) {
         int c=cuts.size();
         cuts.push_back(n);
         cuts.insert(cuts.begin(),0);
         sort(cuts.begin(),cuts.end());
-        vector<vector<int>>dp(c+2,vector<int>(c+2,-1));
-        return minicut(1,c,dp,cuts);
+        // vector<vector<int>>dp(c+2,vector<int>(c+2,-1));
+        // return minicut(1,c,dp,cuts);
+        vector<vector<int>>dp(c+2,vector<int>(c+2,0));
+        return tabu(c,dp,cuts);
     }
 };
