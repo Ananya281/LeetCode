82b9157bc709d20ee154ff21e8f0f2243e040fec
Ananya Chauhan <116284409+Ananya281@users.noreply.github.com>
2025-05-11 00:13:01 +0530

    Time: 78 ms (16.80%) | Memory: 21 MB (53.60%) - LeetSync

--- 1559-cherry-pickup-ii/cherry-pickup-ii.cpp
+++ 1559-cherry-pickup-ii/cherry-pickup-ii.cpp
@@ -42,10 +42,119 @@ public:
         }
         return dp[i][j1][j2]=maxi;
     }
+    int tabu(int n,int m,vector<vector<vector<int>>>&dp,vector<vector<int>>&grid)
+    {
+        for(int j1=0;j1<m;j1++)
+        {
+            for(int j2=0;j2<m;j2++)
+            {
+                if(j1==j2)
+                {
+                    dp[n-1][j1][j2]=grid[n-1][j1];
+                }
+                else
+                {
+                    dp[n-1][j1][j2]=grid[n-1][j1]+grid[n-1][j2];
+                }
+            }
+        }
+        for(int i=n-2;i>=0;i--)
+        {
+            for(int j1=0;j1<m;j1++)
+            {
+                for(int j2=0;j2<m;j2++)
+                {
+                    int maxi=INT_MIN;
+                    for(int d1=-1;d1<=1;d1++)
+                    {
+                        for(int d2=-1;d2<=1;d2++)
+                        {
+                            int ans=INT_MIN;
+                            if(j1==j2)
+                            {
+                                ans=grid[i][j1];
+                            }
+                            else
+                            {
+                                ans=grid[i][j1]+grid[i][j2];
+                            }
+                            if(j1+d1<0 || j1+d1>=m || j2+d2<0 || j2+d2>=m)
+                            {
+                                ans=INT_MIN;
+                            }
+                            else
+                            {
+                                ans=ans+dp[i+1][j1+d1][j2+d2];
+                            }
+                            maxi=max(maxi,ans);
+                        }
+                    }
+                    dp[i][j1][j2]=maxi;
+                }
+            }
+        }
+        return dp[0][0][m-1];
+    }
+    int space(int n,int m,vector<vector<int>>&dp,vector<vector<int>>&grid)
+    {
+        vector<vector<int>>temp(m,vector<int>(m,-1));
+        for(int j1=0;j1<m;j1++)
+        {
+            for(int j2=0;j2<m;j2++)
+            {
+                if(j1==j2)
+                {
+                    dp[j1][j2]=grid[n-1][j1];
+                }
+                else
+                {
+                    dp[j1][j2]=grid[n-1][j1]+grid[n-1][j2];
+                }
+            }
+        }
+        for(int i=n-2;i>=0;i--)
+        {
+            for(int j1=0;j1<m;j1++)
+            {
+                for(int j2=0;j2<m;j2++)
+                {
+                    int maxi=INT_MIN;
+                    for(int d1=-1;d1<=1;d1++)
+                    {
+                        for(int d2=-1;d2<=1;d2++)
+                        {
+                            int ans=INT_MIN;
+                            if(j1==j2)
+                            {
+                                ans=grid[i][j1];
+                            }
+                            else
+                            {
+                                ans=grid[i][j1]+grid[i][j2];
+                            }
+                            if(j1+d1<0 || j1+d1>=m || j2+d2<0 || j2+d2>=m)
+                            {
+                                ans=INT_MIN;
+                            }
+                            else
+                            {
+                                ans=ans+dp[j1+d1][j2+d2];
+                            }
+                            maxi=max(maxi,ans);
+                        }
+                    }
+                    temp[j1][j2]=maxi;
+                }
+            }
+            dp=temp;
+        }
+        return dp[0][m-1];
+    }
     int cherryPickup(vector<vector<int>>& grid) {
         int n=grid.size();
         int m=grid[0].size();
         vector<vector<vector<int>>>dp(n,vector<vector<int>>(m,vector<int>(m,-1)));
-        return memo(0,0,m-1,dp,grid);
+        // return memo(0,0,m-1,dp,grid);
+        return tabu(n,m,dp,grid);
     }
 };
