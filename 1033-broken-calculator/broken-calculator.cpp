2a7bad4327b688aca1df2a6574688aa9c70316a0
Ananya Chauhan <116284409+Ananya281@users.noreply.github.com>
2025-06-08 18:18:29 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (48.32%) - LeetSync

---
+++ 1033-broken-calculator/broken-calculator.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int brokenCalc(int startValue, int target) {
+        if(startValue>=target)
+        {
+            return startValue-target;
+        }
+        if(target%2==0)
+        {
+            return 1+brokenCalc(startValue,target/2);
+        }
+        return 1+brokenCalc(startValue,target+1);
+    }
+};
+
+
+// You avoid exploring all paths — just apply a greedy rule:
+
+// If even → halve it
+
+// If odd → make it even by adding 1
+
