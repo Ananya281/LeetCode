0df9aafaa5fd55f3d6e6623d23d65f70844d18c6
Ananya Chauhan <116284409+Ananya281@users.noreply.github.com>
2025-06-18 10:12:08 +0530

    Time: 75 ms (90.38%) | Memory: 136.8 MB (36.31%) - LeetSync

--- 1962-single-threaded-cpu/single-threaded-cpu.cpp
+++ 1962-single-threaded-cpu/single-threaded-cpu.cpp
@@ -2,39 +2,32 @@ class Solution {
 public:
     vector<int> getOrder(vector<vector<int>>& tasks) {
         int n=tasks.size();
-        // vector<array<int,3>>sorted;
-        // vector<vector<int>>sorted(n,vector<int>(3));
-        vector<vector<int>>sorted;
+        vector<array<int,3>>vec;
         for(int i=0;i<n;i++)
         {
-            int st=tasks[i][0];
-            int pre=tasks[i][1];
-            // sorted.push_back({st,pre,i});
-            // sorted[i][0]=st;
-            // sorted[i][1]=pre;
-            // sorted[i][2]=i;
-            sorted.push_back({st,pre,i});
+            vec.push_back({tasks[i][0],tasks[i][1],i});
         }
-        sort(sorted.begin(),sorted.end());
+        sort(vec.begin(),vec.end());
         vector<int>result;
-        long long curr=0;
-        int ind=0;
-        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
+        priority_queue<pair<long long,long long>,vector<pair<long long,long long>>,greater<pair<long long,long long>>>pq;
+        long long time=0;
+        long long ind=0;
         while(ind<n || !pq.empty())
         {
-            if(pq.empty() && curr<sorted[ind][0])
+            if(pq.empty() && time<vec[ind][0])//cpu idle+no task before
             {
-                curr=sorted[ind][0];
+                time=vec[ind][0];
             }
-            while(ind<n && sorted[ind][0]<=curr)
+            while(ind<n && vec[ind][0]<=time)//cpu idle but task available 
             {
-                pq.push({sorted[ind][1],sorted[ind][2]});
+                pq.push({vec[ind][1],vec[ind][2]});
                 ind++;
             }
-            pair<int,int>currtask=pq.top();
+            long long t=pq.top().first;
+            long long i=pq.top().second;
             pq.pop();
-            curr=curr+currtask.first;
-            result.push_back(currtask.second);
+            time=time+t;
+            result.push_back((int)i);
         }
         return result;
     }
