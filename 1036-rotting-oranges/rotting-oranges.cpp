125f6674594cfbd73d0c056b963d85184c2b5f80
Ananya Chauhan <116284409+Ananya281@users.noreply.github.com>
2025-06-20 11:04:54 +0530

    Time: 2 ms (35.78%) | Memory: 17.2 MB (34.27%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
-    void bfs(queue<pair<int,pair<int,int>>>&q,vector<vector<int>>& grid,vector<vector<int>>&vis,int &time)
+    void bfs(queue<pair<pair<int,int>,int>>&q,vector<vector<int>>&vis,vector<vector<int>>&grid,int &time)
     {
+        vector<int>row={-1,0,+1,0};
+        vector<int>col={0,+1,0,-1};
         int n=grid.size();
         int m=grid[0].size();
-        int drow[4]={-1,0,+1,0};
-        int dcol[4]={0,+1,0,-1};
         while(!q.empty())
         {
-            int r=q.front().second.first;
-            int c=q.front().second.second;
-            int t=q.front().first;
+            int r=q.front().first.first;
+            int c=q.front().first.second;
+            int t=q.front().second;
             time=max(time,t);
             q.pop();
             for(int i=0;i<4;i++)
             {
-                int nrow=r+drow[i];
-                int ncol=c+dcol[i];
-                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && !vis[nrow][ncol] && grid[nrow][ncol]==1)
+                int nrow=r+row[i];
+                int ncol=c+col[i];
+                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && grid[nrow][ncol]==1 && vis[nrow][ncol]!=2)
                 {
                     vis[nrow][ncol]=2;
-                    q.push({t+1,{nrow,ncol}});
+                    q.push({{nrow,ncol},t+1});
                 }
             }
         }
@@ -29,20 +29,20 @@ public:
         int n=grid.size();
         int m=grid[0].size();
         vector<vector<int>>vis(n,vector<int>(m,0));
-        queue<pair<int,pair<int,int>>>q;
+        int time=0;
+        queue<pair<pair<int,int>,int>>q;
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<m;j++)
             {
-                if(!vis[i][j]&&grid[i][j]==2)
+                if(grid[i][j]==2)
                 {
-                    q.push({0,{i,j}});
                     vis[i][j]=2;
+                    q.push({{i,j},0});
                 }
             }
         }
-        int time=0;
-        bfs(q,grid,vis,time);
+        bfs(q,vis,grid,time);
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<m;j++)
