37cad4095e2b23719525ab0536a961a1801f409a
Ananya Chauhan <116284409+Ananya281@users.noreply.github.com>
2025-07-01 11:17:32 +0530

    Time: 0 ms (100.00%) | Memory: 19.1 MB (45.73%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
@@ -16,8 +16,27 @@ public:
         {
             return 0;
         }
-        int left=1+maxDepth(root->left);
-        int right=1+maxDepth(root->right);
-        return max(left,right);
+        queue<TreeNode*>q;
+        q.push(root);
+        int count=0;
+        while(!q.empty())
+        {
+            count++;
+            int s=q.size();
+            for(int i=0;i<s;i++)
+            {
+                TreeNode* node=q.front();
+                q.pop();
+                if(node->left)
+                {
+                    q.push(node->left);
+                }
+                if(node->right)
+                {
+                    q.push(node->right);
+                }
+            }
+        }
+        return count;
     }
 };
