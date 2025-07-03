4885b770fd49ae19dc919f849af135135c9af84e
Ananya Chauhan <116284409+Ananya281@users.noreply.github.com>
2025-07-03 09:46:40 +0530

    Time: 84 ms (29.17%) | Memory: 47.7 MB (35.73%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        int n=nums.size();
-        int count=0;
-        for(int i=0;i<n;i++)
+     int n=nums.size();
+     map<int,int>mpp;
+     mpp[0]=1;
+     int count=0;
+     int sum=0;
+     for(int i=0;i<n;i++)
+     {
+        sum=sum+nums[i];
+        int rem=sum-k;
+        if(mpp.find(rem)!=mpp.end())
         {
-            int sum=0;
-            for(int j=i;j<n;j++)
-            {
-                sum=sum+nums[j];
-                if(sum==k)
-                {
-                    count++;
-                }
-            }
+            count=count+mpp[rem];
         }
-        return count;
+        mpp[sum]=mpp[sum]+1;
+     }   
+     return count;
     }
 };
