051bf44e2050f230190397690ad98a4743466a2c
Ananya Chauhan <116284409+Ananya281@users.noreply.github.com>
2025-07-17 17:38:56 +0530

    Time: 8 ms (95.52%) | Memory: 23.4 MB (96.84%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
@@ -1,37 +1,41 @@
 class Solution {
 public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-        queue<pair<int,pair<int,int>>>q;
         int n=grid.size();
-        if(grid[0][0]==1||grid[n-1][n-1]==1)
+        if(n==0 || grid[0][0]==1 || grid[n-1][n-1]==1)
         {
             return -1;
         }
-        q.push({1,{0,0}});
-        vector<vector<int>>dis(n,vector<int>(n,INT_MAX));
-        dis[0][0]=1;
-        int drow[8]={-1,-1,-1,0,+1,+1,+1,0};
-        int dcol[8]={-1,0,+1,+1,+1,0,-1,-1};
+        queue<pair<int,int>>q;
+        q.push({0,0});
+        grid[0][0]=1;
+        vector<int>row={-1,-1,0,+1,+1,+1,0,-1};
+        vector<int>col={0,+1,+1,+1,0,-1,-1,-1};
+        int level=1;
         while(!q.empty())
         {
-            int d=q.front().first;
-            int r=q.front().second.first;
-            int c=q.front().second.second;
-            q.pop();
-            if(r==n-1 && c==n-1)
+            int s=q.size();
+            for(int i=0;i<s;i++)
             {
-                return d;
-            }
-            for(int i=0;i<8;i++)
-            {
-                int nrow=r+drow[i];
-                int ncol=c+dcol[i];
-                if(nrow>=0 && nrow<n && ncol>=0 && ncol<n && grid[nrow][ncol]==0 && d+1<dis[nrow][ncol])
+                int r=q.front().first;
+                int c=q.front().second;
+                q.pop();
+                if(r==n-1 && c==n-1)
                 {
-                    dis[nrow][ncol]=d+1;
-                    q.push({d+1,{nrow,ncol}});
+                    return level;
+                }
+                for(int i=0;i<8;i++)
+                {
+                    int nrow=r+row[i];
+                    int ncol=c+col[i];
+                    if(nrow>=0 && nrow<n && ncol>=0 && ncol<n && grid[nrow][ncol]==0)
+                    {
+                        grid[nrow][ncol]=1;
+                        q.push({nrow,ncol});
+                    }
                 }
             }
+            level++;
         }
         return -1;
     }
