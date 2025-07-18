93492775d4561bfd259c87bb661f60765db447f0
Ananya Chauhan <116284409+Ananya281@users.noreply.github.com>
2025-07-18 08:17:44 +0530

    Time: 7 ms (27.66%) | Memory: 18.3 MB (49.57%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
@@ -1,39 +1,50 @@
 class Solution {
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-        vector<vector<pair<int,int>>>adj(n);
-        for(auto it:flights)
+        vector<vector<pair<int,int>>>adj(n);//neighbor,cost
+        for(auto it:flights)//u->{v,cost}
         {
             int u=it[0];
             int v=it[1];
-            int c=it[2];
-            adj[u].push_back({v,c});
+            int p=it[2];
+            adj[u].push_back({v,p});
         }
-        queue<pair<int,pair<int,int>>>q;
-        q.push({0,{src,0}});//{stop,{node,cost}}
-        vector<int>dis(n,INT_MAX);
+        queue<pair<int,pair<int,int>>>q;//stops,node,price
+        q.push({0,{src,0}});
+        vector<int>dis(n,1e9);
         dis[src]=0;
         while(!q.empty())
         {
-            int stop=q.front().first;
+            int s=q.front().first;
             int node=q.front().second.first;
-            int cost=q.front().second.second;
+            int p=q.front().second.second;
             q.pop();
             for(auto it:adj[node])
             {
-                int adjnode=it.first;
-                int cw=it.second;
-                if(cost+cw<dis[adjnode]&&stop<=k)
+                int ele=it.first;
+                int price=it.second;
+                if(p+price<dis[ele] && s<=k)
                 {
-                    dis[adjnode]=cost+cw;
-                    q.push({stop+1,{adjnode,dis[adjnode]}});
+                    dis[ele]=p+price;
+                    q.push({s+1,{ele,dis[ele]}});
                 }
             }
         }
-        if(dis[dst]==INT_MAX)
+        if(dis[dst]==1e9)
         {
             return -1;
         }
         return dis[dst];
     }
 };
+
+
+
+//if done using priority_queue, minheap
+//{cost,,currentnode,stopused}
+// TLE occurs
+// if (stops > k || stops >= minStops[node]) continue; 
+// minStops[node] = stops;
+// means:
+// If we’ve used too many stops or we have already reached this node using fewer stops, then skip it.
+// Otherwise, update and go ahead."
