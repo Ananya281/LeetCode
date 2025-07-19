812fdd095501421ba8181557d64ca62326eac5b1
Ananya Chauhan <116284409+Ananya281@users.noreply.github.com>
2025-07-19 22:16:02 +0530

    Time: 14 ms (32.40%) | Memory: 37.4 MB (37.74%) - LeetSync

--- 2090-number-of-ways-to-arrive-at-destination/number-of-ways-to-arrive-at-destination.cpp
+++ 2090-number-of-ways-to-arrive-at-destination/number-of-ways-to-arrive-at-destination.cpp
@@ -6,35 +6,35 @@ public:
         {
             int u=it[0];
             int v=it[1];
-            int time=it[2];
-            adj[u].push_back({v,time});
-            adj[v].push_back({u,time});
+            int wt=it[2];
+            adj[u].push_back({v,wt});
+            adj[v].push_back({u,wt});
         }
+        int mod=1e9+7;
         priority_queue<pair<long long,int>,vector<pair<long long,int>>,greater<pair<long long,int>>>pq;
         pq.push({0,0});
-        int mod=1e9+7;
-        vector<int>path(n,0);
-        path[0]=1;
         vector<long long>dis(n,LLONG_MAX);
+        vector<int>path(n,0);
         dis[0]=0;
+        path[0]=1;
         while(!pq.empty())
         {
-            long long t=pq.top().first;
             int node=pq.top().second;
+            long long d=pq.top().first;
             pq.pop();
             for(auto it:adj[node])
             {
-                int adjnode=it.first;
+                int ele=it.first;
                 int wt=it.second;
-                if(t+wt<dis[adjnode])
+                if(d+wt<dis[ele])
                 {
-                    dis[adjnode]=t+wt;
-                    path[adjnode]=path[node];
-                    pq.push({dis[adjnode],adjnode});
+                    dis[ele]=d+wt;
+                    pq.push({dis[ele],ele});
+                    path[ele]=path[node];
                 }
-                else if(t+wt==dis[adjnode])
+                else if(d+wt==dis[ele])
                 {
-                    path[adjnode]=(path[node]+path[adjnode])%mod;
+                    path[ele]=(path[ele]+path[node])%mod;
                 }
             }
         }
