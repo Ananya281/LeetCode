6d85c35b05297ac24ed051db19029808d04a596d
Ananya Chauhan <116284409+Ananya281@users.noreply.github.com>
2025-07-23 12:53:34 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (17.25%) - LeetSync

---
+++ 678-valid-parenthesis-string/valid-parenthesis-string.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    bool checkValidString(string s) {
+        int n=s.size();
+        stack<int>open;
+        stack<int>ast;
+        for(int i=0;i<n;i++)
+        {
+            if(s[i]=='(')
+            {
+                open.push(i);
+            }
+            else if(s[i]=='*')
+            {
+                ast.push(i);
+            }
+            else
+            {
+                if(!open.empty())
+                {
+                    open.pop();
+                }
+                else if(!ast.empty())
+                {
+                    ast.pop();
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }//*(())(*
+        while(!open.empty() && !ast.empty())
+        {
+            if(open.top()>ast.top())
+            {
+                return false;
+            }
+            open.pop();
+            ast.pop();
+        }
+        if(open.empty())
+        {
+            return true;
+        }
+        return false;
+    }
+};
