beb1480a451d70cf1da992cc3913f11a431f8462
Ananya Chauhan <116284409+Ananya281@users.noreply.github.com>
2025-07-27 17:21:56 +0530

    Time: 1 ms (44.34%) | Memory: 14.9 MB (61.50%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
@@ -2,9 +2,8 @@ class Solution {
 public:
     int memo(int i,int j,vector<vector<int>>&dp,vector<vector<int>>&matrix)
     {
-        int n=matrix.size();
         int m=matrix[0].size();
-        if(j<0 || j>m-1)
+        if(j<0 || j>=m)
         {
             return 1e9;
         }
@@ -16,106 +15,119 @@ public:
         {
             return dp[i][j];
         }
-        int up=matrix[i][j]+memo(i-1,j,dp,matrix);
-        int rd=matrix[i][j]+memo(i-1,j+1,dp,matrix);
-        int ld=matrix[i][j]+memo(i-1,j-1,dp,matrix);
-        dp[i][j]=min({up,rd,ld});
-        return dp[i][j];
+        int top=memo(i-1,j,dp,matrix)+matrix[i][j];
+        int leftdia=memo(i-1,j-1,dp,matrix)+matrix[i][j];
+        int rightdia=memo(i-1,j+1,dp,matrix)+matrix[i][j];
+        return dp[i][j]=min({top,leftdia,rightdia});
     }
     int tabu(int n,int m,vector<vector<int>>&dp,vector<vector<int>>&matrix)
     {
-        for(int j=0;j<m;j++)
+        for(int i=0;i<m;i++)
         {
-            dp[0][j]=matrix[0][j];
+            dp[0][i]=matrix[0][i];
         }
         for(int i=1;i<n;i++)
         {
             for(int j=0;j<m;j++)
             {
-                int up=matrix[i][j]+dp[i-1][j];
-                int rd=matrix[i][j];
-                if((j+1)<m)
+                int top=matrix[i][j];
+                if(i-1>=0)
                 {
-                    rd=rd+dp[i-1][j+1];
+                    top=top+dp[i-1][j];
                 }
                 else
                 {
-                    rd=rd+1e9;
+                    top=top+1e9;
                 }
-                int ld=matrix[i][j];
-                if(j-1>=0)
+                int left=matrix[i][j];
+                if(i-1>=0 && j-1>=0)
                 {
-                    ld=ld+dp[i-1][j-1];
+                    left=left+dp[i-1][j-1];
                 }
                 else
                 {
-                    ld=ld+1e9;
+                    left=1e9;
                 }
-                dp[i][j]=min({up,rd,ld});
+                int right=matrix[i][j];
+                if(i-1>=0 && j+1<n)
+                {
+                    right=right+dp[i-1][j+1];
+                }
+                else
+                {
+                    right=1e9;
+                }
+                dp[i][j]=min({top,left,right});
             }
         }
-        int ans=1e9;
-        for(int j=0;j<m;j++)
+        int result=1e9;
+        for(int i=0;i<m;i++)
         {
-            ans=min(ans,dp[n-1][j]);
+            result=min(result,dp[n-1][i]);
         }
-        return ans;
+        return result;
     }
-    int space(vector<vector<int>>&matrix)
+    int space(int n,int m,vector<int>&dp,vector<vector<int>>&matrix)
     {
-        int n=matrix.size();
-        int m=matrix[0].size();
-        vector<int>prev(m,-1);
-        for(int j=0;j<m;j++)
+        for(int i=0;i<m;i++)
         {
-            prev[j]=matrix[0][j];
+            dp[i]=matrix[0][i];
         }
         for(int i=1;i<n;i++)
         {
-            vector<int>curr(m,-1);
+            vector<int>temp(m,-1);
             for(int j=0;j<m;j++)
             {
-                int up=matrix[i][j]+prev[j];
-                int rd=matrix[i][j];
-                if((j+1)<m)
+                int top=matrix[i][j];
+                if(i-1>=0)
                 {
-                    rd=rd+prev[j+1];
+                    top=top+dp[j];
                 }
                 else
                 {
-                    rd=rd+1e9;
+                    top=top+1e9;
                 }
-                int ld=matrix[i][j];
-                if(j-1>=0)
+                int left=matrix[i][j];
+                if(i-1>=0 && j-1>=0)
                 {
-                    ld=ld+prev[j-1];
+                    left=left+dp[j-1];
                 }
                 else
                 {
-                    ld=ld+1e9;
+                    left=1e9;
                 }
-                curr[j]=min({up,rd,ld});
+                int right=matrix[i][j];
+                if(i-1>=0 && j+1<n)
+                {
+                    right=right+dp[j+1];
+                }
+                else
+                {
+                    right=1e9;
+                }
+                temp[j]=min({top,left,right});
             }
-            prev=curr;
+            dp=temp;
         }
-        int ans=1e9;
-        for(int j=0;j<m;j++)
+        int result=1e9;
+        for(int i=0;i<m;i++)
         {
-            ans=min(ans,prev[j]);
+            result=min(result,dp[i]);
         }
-        return ans;
+        return result;
     }
     int minFallingPathSum(vector<vector<int>>& matrix) {
         int n=matrix.size();
         int m=matrix[0].size();
-        // int ans=1e9;
         vector<vector<int>>dp(n,vector<int>(m,-1));
-        // for(int j=0;j<m;j++)
+        // int ans=1e9;
+        // for(int i=0;i<n;i++)//columns
         // {
-        //     ans=min(ans,memo(n-1,j,dp,matrix));
+        //     ans=min(ans,memo(n-1,i,dp,matrix));
         // }
-        // return tabu(n,m,dp,matrix);
         // return ans;
-        return space(matrix);
+        return tabu(n,m,dp,matrix);
+        // vector<int>dp(m,-1);
+        // return space(n,m,dp,matrix);
     }
 };
