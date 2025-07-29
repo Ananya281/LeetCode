aa995f38ae2d876b35bee28d4cd92363b11ab2c5
Ananya Chauhan <116284409+Ananya281@users.noreply.github.com>
2025-07-29 15:30:34 +0530

    Time: 30 ms (11.29%) | Memory: 30.9 MB (9.39%) - LeetSync

--- 1170-shortest-common-supersequence/shortest-common-supersequence.cpp
+++ 1170-shortest-common-supersequence/shortest-common-supersequence.cpp
@@ -1,7 +1,15 @@
 class Solution {
 public:
-    int tabu(int n,int m,vector<vector<int>>&dp,string str1,string str2)
+    int tabu(int n,int m,string str1,string str2,vector<vector<int>>&dp)
     {
+        for(int i=0;i<=m;i++)
+        {
+            dp[0][i]=0;
+        }
+        for(int j=0;j<=n;j++)
+        {
+            dp[j][0]=0;
+        }
         for(int i=1;i<=n;i++)
         {
             for(int j=1;j<=m;j++)
@@ -23,52 +31,50 @@ public:
     string shortestCommonSupersequence(string str1, string str2) {
         int n=str1.size();
         int m=str2.size();
-        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
-        int len=tabu(n,m,dp,str1,str2);
-        int l=n+m-len;
-        string s="";
-        for(int i=0;i<l;i++)
-        {
-            s=s+'*';
-        }
-        int index=l-1;
+        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
+        int ans=n+m-tabu(n,m,str1,str2,dp);
         int i=n;
         int j=m;
+        int index=ans-1;
+        string str;
+        for(int i=0;i<ans;i++)
+        {
+            str=str+'&';
+        }
         while(i>0 && j>0)
         {
             if(str1[i-1]==str2[j-1])
             {
-                s[index]=str1[i-1];
+                str[index]=str1[i-1];
                 index--;
                 i--;
                 j--;
             }
             else if(dp[i-1][j]>dp[i][j-1])
             {
-
-                s[index]=str1[i-1];
-                index--;
+                str[index]=str1[i-1];
                 i--;
+                index--;
             }
             else
             {
-                s[index]=str2[j-1];
-                index--;
+                str[index]=str2[j-1];
                 j--;
+                index--;
             }
         }
         while(i>0)
         {
-            s[index]=str1[i-1];
-            index--;
+            str[index]=str1[i-1];
             i--;
+            index--;
         }
         while(j>0)
         {
-            s[index]=str2[j-1];
-            index--;
+            str[index]=str2[j-1];
             j--;
+            index--;
         }
-        return s;
+        return str;
     }
 };
