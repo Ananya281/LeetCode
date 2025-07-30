1e7e3af1510fb61737ccc2e8d3ea3fac0e5cd7d6
Ananya Chauhan <116284409+Ananya281@users.noreply.github.com>
2025-07-30 17:10:42 +0530

    Time: 458 ms (18.17%) | Memory: 223.9 MB (28.91%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,106 +1,31 @@
 class Solution {
 public:
-    int memo(int i,int buy, int cap,vector<vector<vector<int>>>&dp,vector<int>&prices)
+    int memo(int i,int state,int capacity, vector<vector<vector<int>>>&dp,vector<int>&prices)
     {
         int n=prices.size();
-        if(i==n || cap==0)
+
+        if(i==n || capacity==0)
         {
             return 0;
         }
-        if(dp[i][buy][cap]!=-1)
+        if(dp[i][state][capacity]!=-1)
         {
-            return dp[i][buy][cap];
+            return dp[i][state][capacity];
         }
         int profit=INT_MIN;
-        if(buy==1)
+        if(state==0)
         {
-            profit=max(-prices[i]+memo(i+1,0,cap,dp,prices),memo(i+1,1,cap,dp,prices));
+            profit=max(-prices[i]+memo(i+1,1,capacity,dp,prices),memo(i+1,0,capacity,dp,prices));
         }
-        else
+        else 
         {
-            profit=max(prices[i]+memo(i+1,1,cap-1,dp,prices),memo(i+1,0,cap,dp,prices));
+            profit=max(prices[i]+memo(i+1,0,capacity-1,dp,prices),memo(i+1,1,capacity,dp,prices));
         }
-        return dp[i][buy][cap]=profit;
-    }
-    int tabu(int n,vector<vector<vector<int>>>&dp,vector<int>&prices)
-    {
-        for(int i=0;i<n;i++)//cap=0
-        {
-            for(int j=0;j<2;j++)
-            {
-                dp[i][j][0]=0;
-            }
-        }
-        for(int i=0;i<2;i++)//last day array end return nothing buy and sell 
-        {
-            for(int j=0;j<3;j++)
-            {
-                dp[n][i][j]=0;
-            }
-        }
-        for(int i=n-1;i>=0;i--)
-        {
-            for(int j=0;j<2;j++)
-            {
-                for(int k=1;k<3;k++)
-                {
-                    int profit=INT_MIN;
-                    if(j==1)
-                    {
-                        profit=max(-prices[i]+dp[i+1][0][k],dp[i+1][1][k]);
-                    }
-                    else
-                    {
-                        profit=max(prices[i]+dp[i+1][1][k-1],dp[i+1][0][k]);
-                    }
-                    dp[i][j][k]=profit;
-                }
-            }
-        }
-        return dp[0][1][2];
-    }
-    int space(int n,vector<vector<int>>&dp,vector<int>&prices) //n*(2*3)
-    {
-        for(int i=0;i<2;i++)
-        {
-            dp[i][0]=0;
-        }
-        for(int i=0;i<2;i++)
-        {
-            for(int j=0;j<3;j++)
-            {
-                dp[i][j]=0;
-            }
-        }
-        for(int i=n-1;i>=0;i--)
-        {
-            vector<vector<int>>temp(2,vector<int>(3,0));
-            for(int j=0;j<2;j++)
-            {
-                for(int k=1;k<3;k++)
-                {
-                    int profit=INT_MIN;
-                    if(j==1)
-                    {
-                        profit=max(-prices[i]+dp[0][k],dp[1][k]);
-                    }else
-                    {
-                        profit=max(prices[i]+dp[1][k-1],dp[0][k]);
-                    }
-                    temp[j][k]=profit;
-                }
-            }
-            dp=temp;
-        }
-        return dp[1][2];
+        return dp[i][state][capacity]=profit;
     }
     int maxProfit(vector<int>& prices) {
         int n=prices.size();
-        // vector<vector<vector<int>>>dp(n,vector<vector<int>>(2,vector<int>(3,-1)));
-        // return memo(0,1,2,dp,prices);
-        // vector<vector<vector<int>>>dp(n+1,vector<vector<int>>(2,vector<int>(3,-1)));
-        // return tabu(n,dp,prices);
-        vector<vector<int>>dp(2,vector<int>(3,-1));
-        return space(n,dp,prices);
+        vector<vector<vector<int>>>dp(n,vector<vector<int>>(2,vector<int>(3,-1)));
+        return memo(0,0,2,dp,prices);
     }
 };
