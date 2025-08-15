7325b94760eeaffe17150a4dcd077cbd3a3d0e56
Ananya Chauhan <116284409+Ananya281@users.noreply.github.com>
2025-08-15 11:36:02 +0530

    Time: 7 ms (37.43%) | Memory: 27.5 MB (37.27%) - LeetSync

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -11,25 +11,26 @@
  */
 class Solution {
 public:
-    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-        map<int,int>mpp;
-        for(int i=0;i<inorder.size();i++)
-        {
-            mpp[inorder[i]]=i;
-        }
-        return buildtree(preorder,0,preorder.size()-1,inorder,0,inorder.size()-1,mpp);
-    }
-    TreeNode* buildtree(vector<int>& preorder, int prestart, int preend, vector<int>& inorder, int instart, int inend, map<int,int>&mpp)
+    TreeNode* build(vector<int>&preorder,int prestart,int preend,vector<int>&inorder,int instart,int inend,map<int,int>&mpp)
     {
-        if(prestart>preend||instart>inend)
+        if(prestart>preend || instart>inend)
         {
             return NULL;
         }
         TreeNode* root=new TreeNode(preorder[prestart]);
         int inroot=mpp[root->val];
         int numleft=inroot-instart;
-        root->left=buildtree(preorder,prestart+1,prestart+numleft,inorder,instart,inroot-1,mpp);
-        root->right=buildtree(preorder,prestart+numleft+1,preend,inorder,inroot+1,inend,mpp);
+        root->left=build(preorder,prestart+1,prestart+numleft,inorder,instart,inroot-1,mpp);
+        root->right=build(preorder,prestart+numleft+1,preend,inorder,inroot+1,inend,mpp);
         return root;
+    }
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        int n=inorder.size();
+        map<int,int>mpp;
+        for(int i=0;i<n;i++)
+        {
+            mpp[inorder[i]]=i;   
+        }
+        return build(preorder,0,preorder.size()-1,inorder,0,inorder.size()-1,mpp);
     }
 };
