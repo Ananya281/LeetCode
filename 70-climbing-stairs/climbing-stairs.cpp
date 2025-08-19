fb6b5ee66692ba9bd73824ea5fb8928c4660c6d6
Ananya Chauhan <116284409+Ananya281@users.noreply.github.com>
2025-08-19 20:38:09 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (37.04%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,48 +1,25 @@
 class Solution {
 public:
-    int memo(vector<int>&dp,int n)
+int memo(int ind,vector<int>&dp)
+{
+    if(ind==0)
     {
-        if(n==0||n==1)
-        {
-            return 1;
-        }
-        if(dp[n]!=-1)
-        {
-            return dp[n];
-        }
-        dp[n]=memo(dp,n-1)+memo(dp,n-2);
-        return dp[n];
+        return 1;
     }
-    int tabu(vector<int>&dp,int n)
+    if(ind==1)
     {
-        dp[0]=dp[1]=1;
-        for(int i=2;i<=n;i++)
-        {
-            dp[i]=dp[i-1]+dp[i-2];
-        }
-        return dp[n];
+        return 1;
     }
-    int space(int n)
+    if(dp[ind]!=-1)
     {
-        int prev2=1;
-        int prev1=1;
-        int curr;
-        if(n==0||n==1)
-        {
-            return 1;
-        }
-        for(int i=2;i<=n;i++)
-        {
-            curr=prev2+prev1;
-            prev2=prev1;
-            prev1=curr;
-        }
-        return prev1;
+        return dp[ind];
     }
+    int ones=memo(ind-1,dp);
+    int twoe=memo(ind-2,dp);
+    return dp[ind]=ones+twoe;
+}
     int climbStairs(int n) {
         vector<int>dp(n+1,-1);
-        // return memo(dp,n);  
-        // return tabu(dp,n);
-        return space(n);      
+        return memo(n,dp);
     }
 };
