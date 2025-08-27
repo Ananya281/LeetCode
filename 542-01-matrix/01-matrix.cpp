e664ffd319412ef6ed956df3521fd4621f5e54fd
Ananya Chauhan <116284409+Ananya281@users.noreply.github.com>
2025-08-27 10:51:36 +0530

    Time: 27 ms (30.27%) | Memory: 41.6 MB (10.64%) - LeetSync

--- 542-01-matrix/01-matrix.cpp
+++ 542-01-matrix/01-matrix.cpp
@@ -1,48 +1,49 @@
 class Solution {
 public:
-    void bfs(queue<pair<pair<int,int>,int>>&q,vector<vector<int>>&dis,vector<vector<int>>&vis,vector<vector<int>>&mat)
+    vector<vector<int>>bfs(queue<pair<int,pair<int,int>>>&q,vector<vector<int>>&mat,vector<vector<int>>&dis,vector<vector<int>>&vis)
     {
+        int row[4]={-1,0,+1,0};
+        int col[4]={0,+1,0,-1};
         int n=mat.size();
         int m=mat[0].size();
-        int drow[4]={-1,0,+1,0};
-        int dcol[4]={0,+1,0,-1};
         while(!q.empty())
         {
-            int r=q.front().first.first;
-            int c=q.front().first.second;
-            int d=q.front().second;
+            int d=q.front().first;
+            int r=q.front().second.first;
+            int c=q.front().second.second;
             q.pop();
             dis[r][c]=d;
             for(int i=0;i<4;i++)
             {
-                int nrow=r+drow[i];
-                int ncol=c+dcol[i];
-                if(nrow>=0&&nrow<n&&ncol>=0&&ncol<m&&!vis[nrow][ncol])
+                int nrow=r+row[i];
+                int ncol=c+col[i];
+                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && !vis[nrow][ncol])
                 {
                     vis[nrow][ncol]=1;
-                    q.push({{nrow,ncol},d+1});
+                    q.push({d+1,{nrow,ncol}});
                 }
             }
         }
+        return dis;
     }
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
         int n=mat.size();
         int m=mat[0].size();
-        vector<vector<int>>vis(n,vector<int>(m));
-        vector<vector<int>>dis(n,vector<int>(m));
-        queue<pair<pair<int,int>,int>>q;
+        vector<vector<int>>vis(n,vector<int>(m,0));
+        vector<vector<int>>dis(n,vector<int>(m,0));
+        queue<pair<int,pair<int,int>>>q;
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<m;j++)
             {
-                if(mat[i][j]==0&&!vis[i][j])
+                if(!vis[i][j] && mat[i][j]==0)
                 {
+                    q.push({0,{i,j}});
                     vis[i][j]=1;
-                    q.push({{i,j},0});
                 }
             }
         }
-        bfs(q,dis,vis,mat);
+        bfs(q,mat,dis,vis);
         return dis;
     }
 };
