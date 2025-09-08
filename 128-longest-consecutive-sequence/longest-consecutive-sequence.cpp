398b49a2e5be7a6d2efd465e33054f9550dc6924
Ananya Chauhan <116284409+Ananya281@users.noreply.github.com>
2025-09-09 00:41:37 +0530

    Time: 191 ms (5.96%) | Memory: 91 MB (16.51%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -6,18 +6,19 @@ public:
         {
             return 0;
         }
-        int longest=1;
-        unordered_set<int>st;
+        set<int>st;
         for(int i=0;i<n;i++)
         {
             st.insert(nums[i]);
         }
+        int longest=1;
         for(auto it:st)
         {
-            if(st.find(it-1)==st.end())
+            int ele=it;
+            if(st.find(ele-1)==st.end())
             {
                 int count=1;
-                int x=it;
+                int x=ele;
                 while(st.find(x+1)!=st.end())
                 {
                     count++;
