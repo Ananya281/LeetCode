cc618bd6929b759f097b4888a9b7ee8e149b50b0
Ananya Chauhan <116284409+Ananya281@users.noreply.github.com>
2025-09-29 18:24:18 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (60.16%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
-    vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>>result;
-        int n=nums.size();
-        int sub=1<<n;
-        for(int i=0;i<sub;i++)
+    void func(int ind,vector<int>&nums,vector<vector<int>>&result,vector<int>&temp)
+    {
+        if(ind==nums.size())
         {
-            vector<int>list;
-            for(int j=0;j<n;j++)
-            {
-                if(i&(1<<j))
-                {
-                    list.push_back(nums[j]);
-                }
-            }
-            result.push_back(list);
+            result.push_back(temp);
+            return;
         }
+        temp.push_back(nums[ind]);
+        func(ind+1,nums,result,temp);
+        temp.pop_back();
+        func(ind+1,nums,result,temp);
+    }
+    vector<vector<int>> subsets(vector<int>& nums) {
+        int n=nums.size();
+        vector<vector<int>>result;
+        vector<int>temp;
+        func(0,nums,result,temp);
         return result;
     }
 };
