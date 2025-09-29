441cce16fc166c43b3c416a500c3d0fad6338e0d
Ananya Chauhan <116284409+Ananya281@users.noreply.github.com>
2025-09-29 12:30:01 +0530

    Time: 36 ms (19.82%) | Memory: 30.8 MB (17.71%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -1,33 +1,33 @@
 class Solution {
 public:
-    string generate(string& str)
+    string generate(string &str)
     {
         vector<int>hash(26,0);
-        int n = str.length();
+        int n=str.size();
         for(int i=0;i<n;i++)
         {
-            char ch = str[i];
-            hash[ch - 'a']++;
+            char ch=str[i];
+            hash[ch-'a']++;
         }
-        string word = "";
+        string word;
         for(int i=0;i<26;i++)
         {
-            int freq = hash[i];
-            if(freq > 0)
+            int freq=hash[i];
+            if(freq>0)
             {
-                word = word + string(freq,i + 'a');
+                word=word+string(freq,i+'a');
             }
         }
         return word;
     }
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        int n = strs.size();
-        map<string,vector<string>>mpp;
+        int n=strs.size();
         vector<vector<string>>result;
+        map<string,vector<string>>mpp;
         for(int i=0;i<n;i++)
         {
-            string str = strs[i];
-            string word = generate(str);
+            string str=strs[i];
+            string word=generate(str);
             mpp[word].push_back(str);
         }
         for(auto it:mpp)
