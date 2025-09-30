ca1949cbff60d614770896fb3ac0da75f072f586
Ananya Chauhan <116284409+Ananya281@users.noreply.github.com>
2025-09-30 16:05:28 +0530

    Time: 33 ms (23.59%) | Memory: 24.4 MB (20.49%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,35 +1,44 @@
 class Solution {
 public:
-    void dfs(int row,int col,vector<vector<char>>&grid,vector<vector<int>>&vis)
+    void bfs(int dr,int dc,vector<vector<char>>&grid,vector<vector<int>>&visited)
     {
-        vis[row][col]=1;
+        int row[4]={-1,0,+1,0};
+        int col[4]={0,+1,0,-1};
         int n=grid.size();
         int m=grid[0].size();
-         int drow[4]={-1,0,+1,0};
-        int dcol[4]={0,+1,0,-1};
+        queue<pair<int,int>>q;
+        q.push({dr,dc});
+        visited[dr][dc]=1;
+        while(!q.empty())
+        {
+            int r=q.front().first;
+            int c=q.front().second;
+            q.pop();
             for(int i=0;i<4;i++)
             {
-                int nrow=row+drow[i];
-                int ncol=col+dcol[i];
-                if(nrow>=0&&nrow<n&&ncol>=0&&ncol<m&&!vis[nrow][ncol]&&grid[nrow][ncol]=='1')
+                int nrow=r+row[i];
+                int ncol=c+col[i];
+                if(nrow>=0 && ncol>=0 && nrow<n && ncol<m && grid[nrow][ncol]=='1' && visited[nrow][ncol]==-1)
                 {
-                    dfs(nrow,ncol,grid,vis);
+                    q.push({nrow,ncol});
+                    visited[nrow][ncol]=1;
                 }
             }
+        }
     }
     int numIslands(vector<vector<char>>& grid) {
         int n=grid.size();
         int m=grid[0].size();
-        vector<vector<int>>vis(n,vector<int>(m));
+        vector<vector<int>>visited(n,vector<int>(m,-1));
         int count=0;
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<m;j++)
             {
-                if(grid[i][j]=='1'&&!vis[i][j])
+                if(grid[i][j]=='1' && visited[i][j]==-1)
                 {
                     count++;
-                    dfs(i,j,grid,vis);
+                    bfs(i,j,grid,visited);
                 }
             }
         }
