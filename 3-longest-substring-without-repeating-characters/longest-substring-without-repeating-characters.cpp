cd609d155ae4678caad230567ab965cc321e0e0e
Ananya Chauhan <116284409+Ananya281@users.noreply.github.com>
2025-10-13 10:51:37 +0530

    Time: 7 ms (64.92%) | Memory: 11.9 MB (55.73%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -1,33 +1,25 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        int hash[256];
-        fill(hash, hash+256, -1);
         int n=s.size();
-        if(n==0)
-        {
-            return 0;
-        }
-        if(n==1)
-        {
-            return 1;
-        }
-        int l=0;
-        int r=0;
+        int left=0;
+        int right=0;
         int maxlen=0;
-        while(r<n)
+        int len=0;
+        map<char,int>mpp;
+        while(right<n)
         {
-            if(hash[s[r]]!=-1)
+            if(mpp.find(s[right])!=mpp.end())
             {
-                if(hash[s[r]]>=l)
+                if(mpp[s[right]]>=left)
                 {
-                    l=hash[s[r]]+1;
+                    left=mpp[s[right]]+1;
                 }
             }
-            int len=r-l+1;
-            maxlen=max(len,maxlen);
-            hash[s[r]]=r;
-            r++;
+            len=right-left+1;
+            maxlen=max(maxlen,len);
+            mpp[s[right]]=right;
+            right++;
         }
         return maxlen;
     }
