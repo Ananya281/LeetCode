d6ae6d9a6ab2efa4c48ee903b33f677b26ce3b17
Ananya Chauhan <116284409+Ananya281@users.noreply.github.com>
2025-10-17 10:43:00 +0530

    Time: 0 ms (100.00%) | Memory: 12.7 MB (26.73%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -1,35 +1,23 @@
 class Solution {
 public:
-    vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>>result;
+    void solve(int ind,vector<int>&nums,vector<int>&curr,vector<vector<int>>&result)
+    {
         int n=nums.size();
-        int subset=1<<n;//2^n n=3 8 subsets total
-        for(int i=0;i<subset;i++)
+        if(ind==n)
         {
-            vector<int>ans;
-            for(int j=0;j<n;j++)
-            {
-                if(i&(1<<j))
-                {
-                    ans.push_back(nums[j]);
-                }
-            }
-            result.push_back(ans);
+            result.push_back(curr);
+            return;
         }
+        solve(ind+1,nums,curr,result);
+        curr.push_back(nums[ind]);
+        solve(ind+1,nums,curr,result);
+        curr.pop_back();
+    }
+    vector<vector<int>> subsets(vector<int>& nums) {
+        int n=nums.size();
+        vector<vector<int>>result;
+        vector<int>curr;
+        solve(0,nums,curr,result);
         return result;
     }
 };
-
-// (1 << j) → ek number jisme sirf j-th bit set hai.
-// j=0 → 0001
-// j=1 → 0010
-// j=2 → 0100
-
-// (i & (1<<j)) check karta hai: kya i ka j-th bit 1 hai?
-// agar 1 hai → matlab subset me nums[j] lena hai.
-// agar 0 hai → skip karna hai.
-
-// i=5 binary=101
-// j=0 (5&1<<0) = 101&001=1 true ans=[10]
-// j=1 false skip
-// j=2 (5&1<<2) = 101&100=4 true ans=[10,30] subset push into result vector
