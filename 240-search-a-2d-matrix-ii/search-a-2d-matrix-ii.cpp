d9aece9555549e38a56110cd6935820dbe830c02
Ananya Chauhan <116284409+Ananya281@users.noreply.github.com>
2025-10-17 10:34:07 +0530

    Time: 55 ms (51.02%) | Memory: 18.7 MB (37.59%) - LeetSync

--- 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.cpp
+++ 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.cpp
@@ -1,5 +1,10 @@
 class Solution {
 public:
+    //matrix are row-sorted and col-sorted but not mentioned that strictly increasing row-wise 
+    //not mentioned that last of row < first of next
+    //can't apply binary search here
+    //when entire matrix behaves like single sorted array, then apply binary search
+    //use Staircase method
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int n=matrix.size();
         int m=matrix[0].size();
