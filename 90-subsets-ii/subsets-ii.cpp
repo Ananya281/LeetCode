37af3214decc27ac8d57e6948b16f2d259982460
Ananya Chauhan <116284409+Ananya281@users.noreply.github.com>
2025-10-17 11:27:13 +0530

    Time: 3 ms (34.58%) | Memory: 11.3 MB (22.74%) - LeetSync

--- 90-subsets-ii/subsets-ii.cpp
+++ 90-subsets-ii/subsets-ii.cpp
@@ -1,38 +1,26 @@
 class Solution {
 public:
-    void backtrack(int ind,vector<int>&nums,vector<int>&curr,vector<vector<int>>&result)
-    {
-        result.push_back(curr);//store current subset
-        //must be called every time the recursive function is visited
-        //because every recursive state itself represents a valid subset
-        for(int j=ind;j<nums.size();j++)
-        {
-            if(j>ind && nums[j]==nums[j-1])//duplicate handling 
-            {
-                continue;
-            }
-            curr.push_back(nums[j]);
-            backtrack(j+1,nums,curr,result);
-            curr.pop_back();
-        }
-    }
+//using bit manipulation
+//need set for storing unique
+//then after completion pushback all the elements from stack to vector
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
         int n=nums.size();
         sort(nums.begin(),nums.end());
-        vector<int>curr;
-        vector<vector<int>>result;
-        backtrack(0,nums,curr,result);
-        return result;
+        int subsets=1<<n;
+        set<vector<int>>st;
+        for(int i=0;i<subsets;i++)
+        {
+            vector<int>subset;
+            for(int j=0;j<n;j++)
+            {
+                if(i & (1<<j))
+                {
+                    subset.push_back(nums[j]);
+                }
+            }
+            st.insert(subset);
+        }
+        vector<vector<int>>result(st.begin(),st.end());
+        return result;   
     }
 };
-
-
-// subsets II, Combination II, Permutations II
-// if (j > i && nums[j] == nums[j - 1]) {
-//     continue;
-// }
-// condition works only after sorting
-// duplicate handling
-// j>i not choosing first element of this level
-// nums[j]==nums[j-1] this element is identical to the previous one
-// then skip it, we already considered that number once in the decision level
