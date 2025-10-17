e4e92ce2cb48a4c5affad9b6f6a318fa044c6d95
Ananya Chauhan <116284409+Ananya281@users.noreply.github.com>
2025-10-17 11:21:06 +0530

    Time: 2 ms (42.00%) | Memory: 10.5 MB (48.75%) - LeetSync

--- 90-subsets-ii/subsets-ii.cpp
+++ 90-subsets-ii/subsets-ii.cpp
@@ -1,25 +1,38 @@
 class Solution {
 public:
-    void recurr(vector<vector<int>>&result,vector<int>&nums,int i,vector<int>&temp)
+    void backtrack(int ind,vector<int>&nums,vector<int>&curr,vector<vector<int>>&result)
     {
-        result.push_back(temp);
-        for(int j=i;j<nums.size();j++)
+        result.push_back(curr);//store current subset
+        //must be called every time the recursive function is visited
+        //because every recursive state itself represents a valid subset
+        for(int j=ind;j<nums.size();j++)
         {
-            if(i!=j && nums[j]==nums[j-1])//duplicacy
+            if(j>ind && nums[j]==nums[j-1])//duplicate handling 
             {
                 continue;
             }
-            temp.push_back(nums[j]);
-            recurr(result,nums,j+1,temp);
-            temp.pop_back();
+            curr.push_back(nums[j]);
+            backtrack(j+1,nums,curr,result);
+            curr.pop_back();
         }
     }
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
         int n=nums.size();
-        vector<vector<int>>result;
-        vector<int>temp;
         sort(nums.begin(),nums.end());
-        recurr(result,nums,0,temp);
+        vector<int>curr;
+        vector<vector<int>>result;
+        backtrack(0,nums,curr,result);
         return result;
     }
 };
+
+
+// subsets II, Combination II, Permutations II
+// if (j > i && nums[j] == nums[j - 1]) {
+//     continue;
+// }
+// condition works only after sorting
+// duplicate handling
+// j>i not choosing first element of this level
+// nums[j]==nums[j-1] this element is identical to the previous one
+// then skip it, we already considered that number once in the decision level
