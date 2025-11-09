dd85286c9ab4c2858aea31e7d47214b92e115c3b
Ananya Chauhan <116284409+Ananya281@users.noreply.github.com>
2025-11-10 00:06:33 +0530

    Time: 3 ms (25.74%) | Memory: 15.1 MB (22.00%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.cpp
+++ 130-surrounded-regions/surrounded-regions.cpp
@@ -2,53 +2,101 @@ class Solution {
 public:
     void bfs(queue<pair<int,int>>&q,vector<vector<int>>&vis,vector<vector<char>>&board)
     {
-        int drow[4]={-1,0,+1,0};
         int n=board.size();
         int m=board[0].size();
+        int drow[4]={-1,0,+1,0};
         int dcol[4]={0,+1,0,-1};
         while(!q.empty())
         {
-            int row=q.front().first;
-            int col=q.front().second;
+            int r=q.front().first;
+            int c=q.front().second;
             q.pop();
+            for(int i=0;i<4;i++)
+            {
+                int nrow=r+drow[i];
+                int ncol=c+dcol[i];
+                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && !vis[nrow][ncol] && board[nrow][ncol]=='O')
+                {
+                    q.push({nrow,ncol});
+                    vis[nrow][ncol]=1;
+                }
+            }
+        }
+    }
+    void dfs(int row,int col,vector<vector<int>>&vis,vector<vector<char>>&board)
+    {
+        int n=board.size();
+        int m=board[0].size();
+        vis[row][col]=1;
+        int drow[4]={-1,0,+1,0};
+        int dcol[4]={0,+1,0,-1};
         for(int i=0;i<4;i++)
         {
             int nrow=row+drow[i];
             int ncol=col+dcol[i];
             if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && !vis[nrow][ncol] && board[nrow][ncol]=='O')
             {
-                q.push({nrow,ncol});
-                vis[nrow][ncol]=1;
+                dfs(nrow,ncol,vis,board);
             }
         }
-        }
     }
-    void solve(vector<vector<char>>& board) 
+    void dfsit(int row,int col,vector<vector<int>>&vis,vector<vector<char>>&board)
     {
-        int n=board.size();//rows
-        int m=board[0].size();//col
-        queue<pair<int,int>>q;
-        vector<vector<int>>vis(n,vector<int>(m,0));
-        for(int i=0;i<n;i++)
+        int n=board.size();
+        int m=board[0].size();
+        stack<pair<int,int>>st;
+        st.push({row,col});
+        vis[row][col]=1;
+        int drow[4]={-1,0,+1,0};
+        int dcol[4]={0,+1,0,-1};
+        while(!st.empty())
         {
-            for(int j=0;j<m;j++)
+            int r=st.top().first;
+            int c=st.top().second;
+            st.pop();
+            for(int i=0;i<4;i++)
             {
-                if(i==0||i==n-1||j==0||j==m-1)
+                int nrow=r+drow[i];
+                int ncol=c+dcol[i];
+                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && !vis[nrow][ncol] && board[nrow][ncol]=='O')
                 {
-                    if(board[i][j]=='O'&&!vis[i][j])
-                    {
-                        q.push({i,j});
-                        vis[i][j]=1;
-                    }
+                    vis[nrow][ncol]=1;
+                    st.push({nrow,ncol});
                 }
             }
         }
-        bfs(q,vis,board);
+    }
+    void solve(vector<vector<char>>& board) {
+        int n=board.size();
+        int m=board[0].size();
+        vector<vector<int>>vis(n,vector<int>(m));
+        for(int i=0;i<n;i++)
+        {
+            if(board[i][0]=='O' && !vis[i][0])
+            {
+                dfsit(i,0,vis,board);
+            }
+            if(board[i][m-1]=='O' && !vis[i][m-1])
+            {
+                dfsit(i,m-1,vis,board);
+            }
+        }
+        for(int i=0;i<m;i++)
+        {
+            if(board[0][i]=='O' && !vis[0][i])
+            {
+                dfsit(0,i,vis,board);
+            }
+            if(board[n-1][i]=='O' && !vis[n-1][i])
+            {
+                dfsit(n-1,i,vis,board);
+            }
+        }
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<m;j++)
             {
-                if(!vis[i][j]&&board[i][j]=='O')
+                if(!vis[i][j] && board[i][j]=='O')
                 {
                     board[i][j]='X';
                     vis[i][j]=1;
